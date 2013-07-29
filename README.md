58f3358b43f63bd55061ee1de69a7b4bb7bab616
Mike Ihbe <mihbe@yammer-inc.com>
2013-07-29 14:07:10 -0700

    Rails skeleton

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+# Dependencies:
+-RVM
+-Postgresql
+
+# Setup:
+
+In a psql console:
+```
+create database fantasysports;
+create user fantasysports with password 'F4n7a5y' superuser;
+```
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fantasysports::Application.config.secret_key_base = 'b8dc322f038ed3306f81dead13443bea7a097e7ba6c905e1e847dc97689e6a701a3fef78c5cb1c6d349a5f63accd76877d4222ff5dd1875601a852077809c467'
