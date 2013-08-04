aed81a52bae8c900c2deb1d48a3bbf828ce03ca9
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-08-04 13:36:57 -0400

    routes, controllers and views for static pages

---
+++ webapp/test/integration/routes_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class RoutesTest < ActionDispatch::IntegrationTest
+
+  test "home route" do
+    assert_routing({path: "/", method: :get}, { controller: "pages", action: "index" })
+  end
+
+  test "terms route" do
+    assert_routing({path: "/terms", method: :get}, { controller: "pages", action: "terms" })
+  end
+
+  test "about route" do
+    assert_routing({path: "/about", method: :get}, { controller: "pages", action: "about" })
+  end
+
+  test "sign_up route" do
+    assert_routing({path: "/sign_up", method: :get}, { controller: "pages", action: "sign_up" })
+  end
+
+end
