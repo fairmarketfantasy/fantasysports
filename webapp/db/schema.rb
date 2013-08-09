382aa68d770391f50ae064d97a685d2886670405
Mike Ihbe <mihbe@yammer-inc.com>
2013-08-09 15:16:29 -0700

    Add exposed_at to markets

--- webapp/db/schema.rb
+++ webapp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130809215005) do
+ActiveRecord::Schema.define(version: 20130809221555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20130809215005) do
     t.datetime "closed_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "exposed_at",      null: false
   end
 
   create_table "players", force: true do |t|
