8c47c5d170cea419c75833f5608e144144d52f38
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-08-08 17:22:32 -0700

    fixing up test for schema change

--- webapp/app/models/game.rb
+++ webapp/app/models/game.rb
@@ -1,11 +1,9 @@
 class Game < ActiveRecord::Base
-  belongs_to :home_team, class_name: "Team", foreign_key: "home_team_id"
-  belongs_to :away_team, class_name: "Team", foreign_key: "away_team_id"
   has_many :stat_events
 
   validates :stats_id, :home_team_id, :away_team_id, :status, :game_day, :game_time, presence: true
 
   def teams
-    Team.where(id: [self.home_team_id, self.away_team_id])
+    Team.where(name: [self.home_team, self.away_team])
   end
 end
--- webapp/app/models/team.rb
+++ webapp/app/models/team.rb
@@ -1,13 +1,11 @@
 class Team < ActiveRecord::Base
   has_many :players
   belongs_to :sport
-  has_many :home_games, class_name: "Game", foreign_key: "home_team_id"
-  has_many :away_games, class_name: "Game", foreign_key: "away_team_id"
 
   validates :sport_id, :abbrev, :name, :conference, :division, presence: true
 
   def games
-    Game.where("home_team_id = #{self.id} OR away_team_id = #{self.id}")
+    Game.where("home_team = #{self.id} OR away_team_id = #{self.id}")
   end
 
 end
