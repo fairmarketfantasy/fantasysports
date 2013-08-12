ed72a1765f379199bc1a00fb47e216e546871dad
Sean Pont <seanpont@gmail.com>
2013-08-11 18:57:51 -0700

    market in progress

--- market/src/main/java/com/mustwin/market/MarketConfiguration.java
+++ market/src/main/java/com/mustwin/market/MarketConfiguration.java
@@ -1,6 +1,7 @@
 package com.mustwin.market;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mustwin.market.pricing.PricingFunction;
 import com.yammer.dropwizard.config.Configuration;
 import com.yammer.dropwizard.db.DatabaseConfiguration;
 
@@ -20,5 +21,16 @@ public class MarketConfiguration extends Configuration {
 
     public DatabaseConfiguration getDatabaseConfiguration() {
         return database;
+    }
+
+    @Valid @NotNull @JsonProperty
+    private PricingFunction pricingFunction;
+
+    public PricingFunction getPricingFunction() {
+        return pricingFunction;
+    }
+
+    public void setPricingFunction(PricingFunction pricingFunction) {
+        this.pricingFunction = pricingFunction;
     }
 }
---
+++ market/src/main/java/com/mustwin/market/core/Contest.java
@@ -0,0 +1,14 @@
+package com.mustwin.market.core;
+
+/**
+ * User: spont200
+ * Date: 8/8/13
+ */
+public class Contest {
+
+    private long id;
+    long marketId;
+    int buyIn;
+
+
+}
