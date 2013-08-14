835c538125559934f3b77ecb8d74f90ff7806ac8
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-08-13 19:26:27 -0700

    markets/:id/contests

--- webapp/app/controllers/markets_controller.rb
+++ webapp/app/controllers/markets_controller.rb
@@ -5,4 +5,17 @@ class MarketsController < ApplicationController
     @markets = Market.page(page)
   end
 
+  def contests
+    if request.get?
+      # Optionally Authenticated. Takes options in this format: {day: ‘2013-07-23’, after: ‘2013-07-22’, page: 1}. 
+      # Default to today’s contests. List all open public contests, 
+      # private contests (that are visible to me if logged in) and in-progress contests matching the criteria
+    elsif request.post?
+      # Authenticated.  This challenges a specific person to a head to head. 
+      # Takes a list of email addresses, a market, a type (“194” is the only type supported for now) and a max number of users.
+      # Create a new, private contest in the market.  Create a roster for this user and this contest.  Charge them for entry.  
+      # Send their friend(s) an email challenging them to join the league and create their own fantasy sports roster.
+    end
+  end
+
 end
--- webapp/test/controllers/markets_controller_test.rb
+++ webapp/test/controllers/markets_controller_test.rb
@@ -5,5 +5,16 @@ class MarketsControllerTest < ActionController::TestCase
     xhr :get, :index
     assert_response :success
     assert assigns(:markets)
+    assert_template 'markets/index'
   end
+
+  # test "get :id/contests" do
+  #   m = markets(:one)
+  #   xhr :get, :contests, {id: m.id}
+  # end
+
+  # test "post :id/contests" do
+  #   m = markets(:one)
+  #   xhr :post, :contests, {id: m.id}
+  # end
 end
