460041000cf2f5fb462281a08dead691b3b0c428
Mike Ihbe <mihbe@yammer-inc.com>
2013-08-13 21:29:09 -0700

    Make updates update attributes on the in memory representations as well

--- data-fetcher/src/nfl/models/types.go
+++ data-fetcher/src/nfl/models/types.go
@@ -7,6 +7,7 @@ import (
   "reflect"
   "log"
   "fmt"
+  "strconv"
 )
 func z() { log.Println("HERE") }
 
@@ -43,6 +44,74 @@ type GamesMarket struct {
   GameStatsId string `model:"key"`
 }
 
+func valueToString(val reflect.Value) string {
+  kind := val.Kind()
+  
+  switch kind {
+  case reflect.Bool:
+    if val.Bool() {
+      return "true"
+    } else {
+      return "false"
+    }
+  case reflect.Int:
+    fallthrough
+  case reflect.Int8:
+    fallthrough
+  case reflect.Int16:
+    fallthrough
+  case reflect.Int32:
+    fallthrough
+  case reflect.Int64:
+    return strconv.Itoa(int(val.Int()))
+  case reflect.Uint:
+    fallthrough
+  case reflect.Uint8:
+    fallthrough
+  case reflect.Uint16:
+    fallthrough
+  case reflect.Uint32:
+    fallthrough
+  case reflect.Uint64:
+    return strconv.Itoa(int(val.Uint()))
+  case reflect.Uintptr:
+    return string(val.Pointer())
+  case reflect.Float32:
+    return strconv.FormatFloat(val.Float(), 'f', 8, 32)
+  case reflect.Float64:
+    return strconv.FormatFloat(val.Float(), 'f', 8, 64)
+  case reflect.Ptr:
+    return string(val.Pointer())
+  case reflect.Slice:
+    return fmt.Sprintf("%s", val.Interface())
+  case reflect.String:
+    return string(val.String())
+  case reflect.UnsafePointer:
+    return string(val.UnsafeAddr())
+  /*case Array:
+    return string(val)
+  case Chan:
+    return string(val)
+  case Func:
+    return string(val)
+  case Interface:
+    return string(val)
+  case Map:
+    return string(val)
+  case Struct:
+    return string(val)
+  case reflect.Complex64:
+    return strconv.FormatFloat(val.Float(), "f", 8, 32)
+    return string(val.Complex())
+  case reflect.Complex128:
+    return string(val.Complex())
+  */
+  default:
+    log.Panicf("Unable to convert value to string %s", val.Interface())
+  }
+  return ""
+}
+
 // This basically arranges things so that this updates changed attributes of unique objects, and only saves them if they didn't exist to begin with
 func (n *NflModel) BeforeSave(db model.Orm, m model.Model) (error, bool) {
   val := reflect.Indirect(reflect.ValueOf(m))
@@ -75,7 +144,6 @@ func (n *NflModel) BeforeSave(db model.Orm, m model.Model) (error, bool) {
 
   // Fetch it
   existing := reflect.New(valType).Interface()
-  
   err := orm.Find(existing)
   if err != nil {
     if err.Error() == "No record found" {
@@ -92,11 +160,21 @@ func (n *NflModel) BeforeSave(db model.Orm, m model.Model) (error, bool) {
     }
     newFieldVal := val.FieldByName(field.Name)
 
-    if newFieldVal.String() != reflect.Zero(field.Type).String() && newFieldVal.String() != existingVal.FieldByName(field.Name).String() {
+
+    // Technically, this is a little janky. We should set these after successful save, maybe do this loop again?
+    // Set fields that were set on the existing object but not the one passed in. This returns "id" and other things.
+    if valueToString(newFieldVal) == valueToString(reflect.Zero(field.Type)) && valueToString(existingVal.FieldByName(field.Name)) != valueToString(reflect.Zero(field.Type)) {
+      log.Printf("Setting val %s to %s\n", field.Name, existingVal.FieldByName(field.Name).Interface())
+      val.FieldByName(field.Name).Set(existingVal.FieldByName(field.Name))
+    }
+
+    // Set fields that are different on the existing object, save it for update
+    if valueToString(newFieldVal) != valueToString(reflect.Zero(field.Type)) && valueToString(newFieldVal) != valueToString(existingVal.FieldByName(field.Name)) {
       existingVal.FieldByName(field.Name).Set(newFieldVal)
     }
   }
-  return orm.Save(existing), false
+  err = orm.Save(existing)
+  return err, false
 }
 
 type Team struct {
