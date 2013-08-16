6575bb14b9e7d80264cd07abc52b69e3b287ba90
Mike Ihbe <mihbe@yammer-inc.com>
2013-08-16 01:10:22 -0700

    More god

--- webapp/config/deploy/god.rb
+++ webapp/config/deploy/god.rb
@@ -6,10 +6,10 @@ God.watch do |w|
   w.name = "puma"
   w.start = "bundle exec puma -t 0:16 -w 2 -e production -b unix://#{BASE_DIR}/shared/tmp/puma.sock --pidfile #{PID_PATH}/puma.pid"
   w.dir = BASE_DIR + '/current/webapp'
-  w.log = BASE_DIR + '/shared/log/access.log'
+  w.log = BASE_DIR + '/shared/log/puma.log'
   w.stop          = "kill -s TERM $(cat #{PID_PATH}/puma.pid)"
   w.restart       = "kill -s USR2 $(cat #{PID_PATH}/puma.pid)"
-  w.pid_file      = PID_PATH
+  w.pid_file      = PID_PATH + "/puma.pid"
   w.start_grace   = 20.seconds
   w.restart_grace = 20.seconds
   w.keepalive#(:memory_max => 150.megabytes, :cpu_max => 50.percent)
