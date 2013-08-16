8757e96e66c53071ac7b39a594e8dcf7b3328e6d
Mike Ihbe <mihbe@yammer-inc.com>
2013-08-16 12:14:25 -0700

    Fix css compression, by turning it off

--- webapp/config/environments/production.rb
+++ webapp/config/environments/production.rb
@@ -33,7 +33,7 @@ Fantasysports::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :less
+  # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
