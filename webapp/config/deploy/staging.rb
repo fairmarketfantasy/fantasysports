a3d4582e4f1b24d6e31b51525c07fde879261d31
Mike Ihbe <mihbe@yammer-inc.com>
2013-08-16 16:48:18 -0700

    Shit

--- webapp/config/deploy/staging.rb
+++ webapp/config/deploy/staging.rb
@@ -1,5 +1,5 @@
 set :stage_name, 'staging'
 set :rails_env, 'staging'
 
-server "staging.redpenpal.com", :app, :web, :primary => true     # Your HTTP server
+#server "staging.redpenpal.com", :app, :web, :primary => true     # Your HTTP server
 
