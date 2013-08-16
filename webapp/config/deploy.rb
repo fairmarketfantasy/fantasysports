f9276c070d98cfef595bff1c910214893182391b
Mike Ihbe <mihbe@yammer-inc.com>
2013-08-16 16:48:18 -0700

    Shit

--- webapp/config/deploy.rb
+++ webapp/config/deploy.rb
@@ -1,53 +1,39 @@
-set :application, "redpen"
+require 'pp'
+require 'chef'
+require 'chef/rest'
+require 'chef/search/query'
+require 'debugger'
 
-set :stages, %w(production staging)
-set :default_stage, "staging"
-
-require 'capistrano/ext/multistage'
-
-set :repository, "git@github.com:MustWin/redpen.git"
-set :deploy_via, :remote_cache     # Avoid full repo clones
-set :branch, :master
 
 set :user, "ubuntu"             # The server's user for deploys
-set :deploy_to, "/www/redpen"
-
-set :rvm_ruby_string, 'ruby-1.9.3-p286@redpen'
-set :rvm_type, :system
-require 'rvm/capistrano'
-
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, "fantasysports"
+set :stages, %w(production staging)
+set :default_stage, "staging"
+require 'capistrano/ext/multistage'
 
 ssh_options[:forward_agent] = true
 default_run_options[:pty] = true  # Must be set for the password prompt
                                   # from git to work
 
-# if you want to clean up old releases on each deploy uncomment this:
-after "deploy:restart", "deploy:cleanup"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+# Don't do any normal shit, instead, just run chef client on the matching hosts
 
-# If you are using Passenger mod_rails uncomment this:
+Chef::Config.from_file(File.expand_path("~/chef-repo/.chef/knife.rb"))
+query = Chef::Search::Query.new
+#query_string = "cluster_name:dip AND chef_environment:" + env + ' AND run_list:role\[tasktracker\]'
+query_string = "recipes:fantasysports"
+nodes = query.search('node', query_string).first rescue []
+debugger
+pp nodes
+role :app, *nodes.map(&:name).flatten
+
 namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    # Pkill returns 1 when there are no processes that match
-    #run "cd #{release_path}; bundle exec god stop scheduler" rescue nil
-    #run "cd #{release_path}; bundle exec god stop workers" rescue nil
-    run "cd #{release_path}; bundle exec god stop puma" rescue nil
-    run "cd #{release_path}; bundle exec god stop search" rescue nil
-    run "pkill -f god" rescue nil
-    sleep 10
-    run "cd #{release_path}; bundle exec god -c config/deploy/god_#{stage_name}.rb"
-    puts "Optimistically waiting 45 seconds for puma to start..."
-    sleep 45
-    run "sudo nginx -s reload"
-  end
+ task :start do ; end
+ task :stop do ; end
+ task :restart do ; end
+ task :update_code do 
+    run "sudo chef-client"
+ end #override this task to prevent capistrano to upload on servers
+ task :symlink do ; end #don't create the current symlink to the last release
 end
-
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
