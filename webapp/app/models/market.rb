2396059f97f4ab8c3707ebbb144eac278ce93bd0
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-08-17 02:44:40 -0700

    get rid of superfluous scope

--- webapp/app/models/market.rb
+++ webapp/app/models/market.rb
@@ -4,7 +4,6 @@ class Market < ActiveRecord::Base
 
   validates :shadow_bets, :shadow_bet_rate, presence: true
 
-  scope :order_closed_asc,  ->       { order('closed_at asc') }
   scope :opened_after,      ->(time) { where("opened_at > ?", time) }
   scope :closed_after,      ->(time) { where('closed_at > ?', time) }
 
--- webapp/test/models/market_test.rb
+++ webapp/test/models/market_test.rb
@@ -23,7 +23,7 @@ class MarketTest < ActiveSupport::TestCase
       describe "chain them all together" do
         it "should produce valid SQL" do
           assert_nothing_raised do
-            Market.opened_after(Time.now).closed_after(Time.now).page(1).order_closed_asc.inspect
+            Market.opened_after(Time.now).closed_after(Time.now).page(1).order('closed_at asc').inspect
           end
         end
       end
