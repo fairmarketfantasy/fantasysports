6301f7de105c3a45b12158ed7262406f88082fec
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-08-25 17:32:29 -0700

    login and get a sessoin without facebook, with only an email and password

---
+++ webapp/test/controllers/devise_sessions_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class Devise::SessionsControllerTest < ActionController::TestCase
+
+  setup do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @user = create :user
+  end
+
+  test "post to sessoins create with the correct password" do
+    xhr :post, :create, user: {email: @user.email, password: "123456"}
+    assert_equal @user, @controller.current_user
+  end
+
+  test "post to sessoins create with the wrong password" do
+    xhr :post, :create, user: {email: @user.email, password: "123"}
+    assert_nil @controller.current_user
+  end
+end
