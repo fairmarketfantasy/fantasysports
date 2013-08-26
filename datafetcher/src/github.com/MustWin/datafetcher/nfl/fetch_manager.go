93f32d6bf188a6572490a185df2edd7ad59160f5
Sean Pont <seanpont@gmail.com>
2013-08-26 08:54:26 -0700

    directions on how to compile and run datafetcher in background

--- datafetcher/src/github.com/MustWin/datafetcher/nfl/fetch_manager.go
+++ datafetcher/src/github.com/MustWin/datafetcher/nfl/fetch_manager.go
@@ -1,177 +1,175 @@
 package nfl
 
 import (
-  "github.com/MustWin/datafetcher/lib"
-  "github.com/MustWin/datafetcher/lib/model"
-  "github.com/MustWin/datafetcher/nfl/models"
-  "time"
-  "log"
+	"github.com/MustWin/datafetcher/lib"
+	"github.com/MustWin/datafetcher/lib/model"
+	"github.com/MustWin/datafetcher/nfl/models"
+	"log"
+	"time"
 )
-
 
 var NflSeasons = []string{"PRE", "REG", "PST"}
 
-
 type FetchManager struct {
-  lib.FetchManagerBase
-  Fetcher Fetcher
-  Orm model.Orm
+	lib.FetchManagerBase
+	Fetcher Fetcher
+	Orm     model.Orm
 }
 
 func (mgr *FetchManager) Startup() error {
-  return mgr.Daily()
+	return mgr.Daily()
 }
 
 func (mgr *FetchManager) Daily() error {
-  // Refresh all games for each season
-  /*games := make([]*models.Game, 0)
-  for _, seasonType := range(NflSeasons) {
-    mgr.Fetcher.NflSeason = seasonType
-    games = append(games, mgr.refreshGames()...)
-  }
+	// Refresh all games for each season
+	/*games := make([]*models.Game, 0)
+	  for _, seasonType := range(NflSeasons) {
+	    mgr.Fetcher.NflSeason = seasonType
+	    games = append(games, mgr.refreshGames()...)
+	  }
 
-  //lib.PrintPtrs(games)
+	  //lib.PrintPtrs(games)
 
-// Set the fetcher to the correct dates / seasons, etc
+	// Set the fetcher to the correct dates / seasons, etc
 
-  mgr.refreshFetcher(games)
-*/
-  // Grab the latest standings for this season
-  teams := mgr.refreshStandings()
+	  mgr.refreshFetcher(games)
+	*/
+	// Grab the latest standings for this season
+	teams := mgr.refreshStandings()
 
-  // Refresh rosters for each team
-  for _, team := range(teams) {
-    mgr.refreshTeamRosters(team.Abbrev)
-  }
-/*
-  // Schedule jobs to collect play stats
-  for _, game := range(games) {
-    mgr.schedulePbpCollection(game)
-  }
+	// Refresh rosters for each team
+	for _, team := range teams {
+		mgr.refreshTeamRosters(team.Abbrev)
+	}
+	/*
+	   // Schedule jobs to collect play stats
+	   for _, game := range(games) {
+	     mgr.schedulePbpCollection(game)
+	   }
 
-  // Create markets for
-  mgr.createMarkets(games)
-*/
-  return nil
+	   // Create markets for
+	   mgr.createMarkets(games)
+	*/
+	return nil
 }
 
 // TODO: Remove this, it will be handled by the publish task
 func (mgr *FetchManager) savePlayersForMarket(market models.Market, teamAbbrev string) {
-  var players []models.Player
-  err := mgr.Orm.GetDb().Where("team = $1", teamAbbrev).FindAll(&players)
-  if err != nil {
-    log.Println(err)
-  }
-  for _, player := range(players) {
-    mktPlayer := models.MarketPlayer{MarketId: market.Id, PlayerId: player.Id, InitialPrice: 1000.0}
-    mgr.Orm.GetDb().Save(&mktPlayer)
-  }
+	var players []models.Player
+	err := mgr.Orm.GetDb().Where("team = $1", teamAbbrev).FindAll(&players)
+	if err != nil {
+		log.Println(err)
+	}
+	for _, player := range players {
+		mktPlayer := models.MarketPlayer{MarketId: market.Id, PlayerId: player.Id, InitialPrice: 1000.0}
+		mgr.Orm.GetDb().Save(&mktPlayer)
+	}
 }
 
 func (mgr *FetchManager) createMarkets(games []*models.Game) {
-  possibleMarkets := make(map[string][]*models.Game, 0)
-  for i := 0; i < len(games); i++ {
-    key := games[i].GameDay.String()
-    _, found := possibleMarkets[key]
-    if !found {
-      possibleMarkets[key] = make([]*models.Game, 0)
-    }
-    possibleMarkets[key] = append(possibleMarkets[key], games[i])
-  }
-  for _, daysGames := range(possibleMarkets) {
-    if len(daysGames) > 1 {
-      market := models.Market{}
-      market.ShadowBets = 1000
-      market.ShadowBetRate = 0.75
-      market.PublishedAt = daysGames[0].GameDay.Add(-6 * 24 * time.Hour)
-      market.OpenedAt = daysGames[0].GameDay.Add(-6 * 24 * time.Hour)
-      market.ClosedAt = daysGames[0].GameTime.Add(-5 * time.Minute) // DO NOT CHANGE THIS WITHOUT REMOVING ALREADY CREATED BUT UNUSED MARKETS
-      log.Printf("Creating a market closing on %s with %d games", market.ClosedAt, len(daysGames))
-      mgr.Orm.Save(&market)
-      for _, game := range(daysGames) {
-        mktGame := models.GamesMarket{GameStatsId: game.StatsId, MarketId: market.Id}
-        mgr.Orm.Save(&mktGame)
-        mgr.savePlayersForMarket(market, game.HomeTeam)
-        mgr.savePlayersForMarket(market, game.AwayTeam)
-      }
-    }
-  }
+	possibleMarkets := make(map[string][]*models.Game, 0)
+	for i := 0; i < len(games); i++ {
+		key := games[i].GameDay.String()
+		_, found := possibleMarkets[key]
+		if !found {
+			possibleMarkets[key] = make([]*models.Game, 0)
+		}
+		possibleMarkets[key] = append(possibleMarkets[key], games[i])
+	}
+	for _, daysGames := range possibleMarkets {
+		if len(daysGames) > 1 {
+			market := models.Market{}
+			market.ShadowBets = 1000
+			market.ShadowBetRate = 0.75
+			market.PublishedAt = daysGames[0].GameDay.Add(-6 * 24 * time.Hour)
+			market.OpenedAt = daysGames[0].GameDay.Add(-6 * 24 * time.Hour)
+			market.ClosedAt = daysGames[0].GameTime.Add(-5 * time.Minute) // DO NOT CHANGE THIS WITHOUT REMOVING ALREADY CREATED BUT UNUSED MARKETS
+			log.Printf("Creating a market closing on %s with %d games", market.ClosedAt, len(daysGames))
+			mgr.Orm.Save(&market)
+			for _, game := range daysGames {
+				mktGame := models.GamesMarket{GameStatsId: game.StatsId, MarketId: market.Id}
+				mgr.Orm.Save(&mktGame)
+				mgr.savePlayersForMarket(market, game.HomeTeam)
+				mgr.savePlayersForMarket(market, game.AwayTeam)
+			}
+		}
+	}
 }
 
 // Assumes games are in chronological order now
 func (mgr *FetchManager) refreshFetcher(games []*models.Game) {
-  now := time.Now()
-  for i :=0; i < len(games); i++ {
-    if now.After(games[i].GameTime) {
-      mgr.Fetcher.NflSeason = games[i].SeasonType
-      mgr.Fetcher.NflSeasonWeek = games[i].SeasonWeek
-      mgr.Fetcher.Year = games[0].SeasonYear
-      break
-    }
-  }
+	now := time.Now()
+	for i := 0; i < len(games); i++ {
+		if now.After(games[i].GameTime) {
+			mgr.Fetcher.NflSeason = games[i].SeasonType
+			mgr.Fetcher.NflSeasonWeek = games[i].SeasonWeek
+			mgr.Fetcher.Year = games[0].SeasonYear
+			break
+		}
+	}
 }
 
 func (mgr *FetchManager) refreshStandings() []*models.Team {
-  log.Println("Fetching teams")
-  teams := mgr.Fetcher.GetStandings()
-  mgr.Orm.SaveAll(teams)
-  return teams
+	log.Println("Fetching teams")
+	teams := mgr.Fetcher.GetStandings()
+	mgr.Orm.SaveAll(teams)
+	return teams
 }
 
 func (mgr *FetchManager) refreshGames() []*models.Game {
-  log.Println("Fetching games")
-  games := mgr.Fetcher.GetSchedule()
-  mgr.Orm.SaveAll(games)
-  return games
+	log.Println("Fetching games")
+	games := mgr.Fetcher.GetSchedule()
+	mgr.Orm.SaveAll(games)
+	return games
 }
 
 func (mgr *FetchManager) refreshTeamRosters(team string) {
-  log.Printf("Fetching %s players", team)
-  players := mgr.Fetcher.GetTeamRoster(team)
-  mgr.Orm.SaveAll(players)
+	log.Printf("Fetching %s players", team)
+	players := mgr.Fetcher.GetTeamRoster(team)
+	mgr.Orm.SaveAll(players)
 }
 
 func (mgr *FetchManager) refreshGameStatistics(game *models.Game) {
-  log.Printf("Fetching stats for game %s", game.StatsId)
-  // EPIC FUCKING TODO: don't save all of these every time, add a cache layer that checks to see if they're updated
-  stats:= mgr.Fetcher.GetGameStatistics(game.AwayTeam, game.HomeTeam)
-  mgr.Orm.SaveAll(stats)
+	log.Printf("Fetching stats for game %s", game.StatsId)
+	// EPIC FUCKING TODO: don't save all of these every time, add a cache layer that checks to see if they're updated
+	stats := mgr.Fetcher.GetGameStatistics(game.AwayTeam, game.HomeTeam)
+	mgr.Orm.SaveAll(stats)
 }
 
 func (mgr *FetchManager) schedulePbpCollection(game *models.Game) {
-  POLLING_PERIOD := 10 * time.Second
-  currentSequenceNumber := -1
-  gameover := false
-  if game.GameTime.After(time.Now()) {
-    var poll = func(){}
-    poll = func() {
-      dirty := false
-      gameEvents := mgr.Fetcher.GetPlayByPlay(game.AwayTeam, game.HomeTeam)
-      for i, event := range(gameEvents) {
-        if event.SequenceNumber < currentSequenceNumber {
-          continue
-        }
-        mgr.Orm.Save(event)
-        currentSequenceNumber = i
-        dirty = true
-        if event.Type == "gameover" {
-          gameover = true
-        }
-      }
-      if dirty {
-        mgr.refreshGameStatistics(game)
-        if gameover {
-          mgr.refreshStandings()
-          // And refresh 'em again later just in case
-          time.AfterFunc(1 * time.Minute, func() { mgr.refreshStandings() })
-          time.AfterFunc(10 * time.Minute, func() { mgr.refreshStandings() })
-          return
-        }
-      }
-      time.AfterFunc(POLLING_PERIOD, poll)
-    }
-    mgr.Schedule("nfl-pbp-" + game.StatsId, game.GameTime.Add(-5*time.Minute), poll)
-  }
+	POLLING_PERIOD := 10 * time.Second
+	currentSequenceNumber := -1
+	gameover := false
+	if game.GameTime.After(time.Now()) {
+		var poll = func() {}
+		poll = func() {
+			dirty := false
+			gameEvents := mgr.Fetcher.GetPlayByPlay(game.AwayTeam, game.HomeTeam)
+			for i, event := range gameEvents {
+				if event.SequenceNumber < currentSequenceNumber {
+					continue
+				}
+				mgr.Orm.Save(event)
+				currentSequenceNumber = i
+				dirty = true
+				if event.Type == "gameover" {
+					gameover = true
+				}
+			}
+			if dirty {
+				mgr.refreshGameStatistics(game)
+				if gameover {
+					mgr.refreshStandings()
+					// And refresh 'em again later just in case
+					time.AfterFunc(1*time.Minute, func() { mgr.refreshStandings() })
+					time.AfterFunc(10*time.Minute, func() { mgr.refreshStandings() })
+					return
+				}
+			}
+			time.AfterFunc(POLLING_PERIOD, poll)
+		}
+		mgr.Schedule("nfl-pbp-"+game.StatsId, game.GameTime.Add(-5*time.Minute), poll)
+	}
 }
 
 /*
