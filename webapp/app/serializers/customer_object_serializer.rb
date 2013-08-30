7c95b2f7badbd9ebe57c6f6deb3b49097e52b166
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-08-30 11:02:53 -0700

    saving credit cards and getting it back on the customer object

---
+++ webapp/app/serializers/customer_object_serializer.rb
@@ -0,0 +1,15 @@
+class CustomerObjectSerializer < ActiveModel::Serializer
+  attributes :id, :balance, :locked, :locked_reason, :cards
+
+  def cards
+    stripe_obj = object.retrieve
+    stripe_obj.cards.data.map do |card|
+      { id:          card.id,
+        last4:       card.last4,
+        type:        card.type,
+        exp_month:   card.exp_month,
+        exp_year:    card.exp_year,
+        address_zip: card.address_zip }
+    end
+  end
+end
