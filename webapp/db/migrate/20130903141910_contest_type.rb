888998c6671f84bc59b318437337e85050cf936d
Sean Pont <seanpont@gmail.com>
2013-09-03 08:57:31 -0700

    added contest type table

---
+++ webapp/db/migrate/20130903141910_contest_type.rb
@@ -0,0 +1,19 @@
+class ContestType < ActiveRecord::Migration
+  def change
+  	create_table :contest_types do |t|
+  		t.integer :market_id, :null => false
+  		t.string :name, :null => false
+  		t.text :description, :null => true
+  		t.integer :max_entries, :null => false
+  		t.integer :buy_in, :null => false
+  		t.decimal :rake, :null => false
+  		t.text :payout_structure, :null => false
+  		t.integer :user_id, :null => true
+  		t.boolean :private, :null => true
+  	end
+  	remove_column :rosters, :contest_type, :string
+  	add_column :rosters, :contest_type_id, :integer, :null => false
+  	add_index :rosters, :contest_type_id
+  	add_index :contest_types, :market_id
+  end
+end
