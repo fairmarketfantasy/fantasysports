65414366b3173c52a3d905056f0cd5b0fa0adaf9
Mike Ihbe <mihbe@yammer-inc.com>
2013-09-03 20:08:50 -0700

    Attempting to get flatui assets to compile in prod

--- webapp/config/environments/staging.rb
+++ webapp/config/environments/staging.rb
@@ -68,7 +68,7 @@ Fantasysports::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( app.css eeze.css )
+  config.assets.precompile += %w( flat-ui.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
