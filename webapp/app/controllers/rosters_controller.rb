2f324511912076518d07a0eef8871eb1f149270d
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-09-04 19:14:05 -0700

    front end needs to pass through contest_type_id to the joinContest function

--- webapp/app/controllers/rosters_controller.rb
+++ webapp/app/controllers/rosters_controller.rb
@@ -3,7 +3,8 @@ class RostersController < ApplicationController
   # Create a roster for a contest type
   def create
     market = Market.find(params[:market_id])
-    roster = Roster.generate_contest_roster(current_user, market, params[:contest_type], params[:buy_in])
+    contest_type = ContestType.find(params[:contest_type_id])
+    roster = Roster.generate_contest_roster(current_user, market, contest_type, params[:buy_in])
     render_api_response roster
   end
 
---
+++ webapp/test/controllers/rosters_controller_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class RostersControllerTest < ActionController::TestCase
+  setup do
+    setup_simple_market
+    sign_in(create(:user))
+    @ct = create(:contest_type)
+  end
+
+  test "post /rosters" do
+    assert_difference("Roster.count", 1) do
+      xhr :post, :create, {market_id: @market.id, contest_type_id: @ct.id, buy_in: 2}
+    end
+    assert_response :success
+  end
+
+end
