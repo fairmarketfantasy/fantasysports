8545074508b0ffd726c9d21a15c8078a3359dbfa
Wil Everts <your@cousinwil.com>
2013-09-05 10:08:09 -0700

    have the base template in, the start of the landing page, and the base style guide

--- webapp/app/controllers/pages_controller.rb
+++ webapp/app/controllers/pages_controller.rb
@@ -4,10 +4,18 @@ class PagesController < ApplicationController
   def index
   end
 
+  def landing
+    render layout: "landing"
+  end
+
   def terms
   end
 
   def about
+  end
+
+  def guide
+  	render layout: "guide"
   end
 
 end
