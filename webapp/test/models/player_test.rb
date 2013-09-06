4ec52e34ce1d46e15704126bc6ae499f1351a168
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-09-06 10:18:57 -0700

    kill off more fixtures passing tests

--- webapp/test/models/player_test.rb
+++ webapp/test/models/player_test.rb
@@ -1,44 +1,61 @@
 require 'test_helper'
 
 class PlayerTest < ActiveSupport::TestCase
+
   describe Player do
-    let(:mj) { Player.find_by(name: "Michael Jordan") }
+
+    describe ".autocomplete" do
+
+      let(:mcfadden) { create(:player, name: "Darren McFadden", position: 'RB') }
+
+      it "should take a string and return the right results" do
+        Player.autocomplete("Darr").must_include(mcfadden)
+      end
+
+      it "should return results for last name" do
+        Player.autocomplete("McF").must_include(mcfadden)
+      end
+
+      it "should return results for a lowercase name" do
+        Player.autocomplete("darr").must_include(mcfadden)
+      end
+
+      it "should not return results when there is no match" do
+        Player.autocomplete("Noone is here").must_be_empty
+      end
+
+    end
 
     describe "scopes" do
-      describe ".autocomplete" do
-        it "should take a string and return the right results" do
-          Player.autocomplete("Mich").must_include(mj)
-        end
 
-        it "should return results for last name" do
-          Player.autocomplete("Jordan").must_include(mj)
-        end
-
-        it "should return results for a lowercase name" do
-          Player.autocomplete("mich").must_include(mj)
-        end
-
-        it "should not return results when there is no match" do
-          Player.autocomplete("Noone is here").must_be_empty
-        end
+      before(:all) do
+        setup_simple_market
       end
+
+      let(:player1) { @team1.players.first }
+      let(:player2) { @team2.players.first }
 
       describe ".on_team" do
-        let(:team) { teams(:one) }
+        let(:result)  { Player.on_team(@team1) }
+
         it "should return players on a team" do
-          Player.on_team(team).must_include(mj)
+          result.must_include(player1)
+          result.wont_include(player2)
         end
       end
 
-      # describe ".in_contest" do
-      # end
+    #   # # describe ".in_contest" do
+    #   # # end
 
       describe ".in_game" do
-        let(:game) { create(:game, home_team: 'BLS', away_team: 'BIS') }
+        let(:result)  { Player.in_game(@game) }
+
         it "should return players in a game" do
-          Player.in_game(game).must_include(mj)
+          result.must_include(player1)
+          result.must_include(player2)
         end
       end
+
     end
   end
 end
