d188d78bddd434a070d3847a4de441635aa8ab94
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-09-07 02:11:35 -0700

    using angulars ui-bootstrap modals to trigger dialog
    * for sign up modal

--- webapp/app/assets/javascripts/controllers/login_controller.js
+++ webapp/app/assets/javascripts/controllers/login_controller.js
@@ -1,16 +1,30 @@
 angular.module("app.controllers")
-.controller('LoginController', ['$scope', 'fs', function($scope, fs) {
+.controller('LoginController', ['$scope', 'fs', '$dialog', function($scope, fs, $dialog) {
   // setup fb signup buttons
   var serviceSizes = {
     facebook: 'height=460,width=730',
   /*  linkedin: 'height=260,width=630', // customize these
     google: 'height=260,width=630'*/
   };
+  $scope.signUpModal = function(){
+    var dialogOpts = {
+          backdrop: true,
+          keyboard: true,
+          backdropClick: true,
+          dialogClass: 'modal',
+          templateUrl: '/assets/sign_up_dialog.html',
+          controller: 'SignUpDialogController'
+        };
+
+     var d = $dialog.dialog(dialogOpts);
+     d.open();
+  };
   $scope.user = $scope.user || {};
   $scope.login = function(service) {
     if(service === 'email'){
       fs.user.login($scope.user).then(function(resp){
-        window.setCurrentUser(resp);
+        // window.setCurrentUser(resp);
+        window.location.reload(true);
       });
     } else {
       window.open('/users/auth/' + service, '', serviceSizes[service]);
