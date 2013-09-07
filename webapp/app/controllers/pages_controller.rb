e51bbda441d5a7aea630e87a7e58f7ce3384b465
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-09-07 00:06:44 -0700

    handle pages#index for signed in and signed out users
    * When signed in render default layout
    * When not signed in, render layout: 'landing'
    * Sign out via :get request instead of DELETE, because in order to make
    link_to work with a DELETE request, we need jquery_ujs 394 lines of js,
    figuered this was better...

--- webapp/app/controllers/pages_controller.rb
+++ webapp/app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   skip_before_filter :authenticate_user!
 
   def index
+    unless current_user
+      render layout: 'landing'
+    end
   end
 
   def landing
--- webapp/app/controllers/users/sessions_controller.rb
+++ webapp/app/controllers/users/sessions_controller.rb
@@ -7,6 +7,11 @@ class Users::SessionsController < Devise::SessionsController
     render json: UserSerializer.new(current_user, scope: current_user)
   end
 
+  def destroy
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    redirect_to root_url
+  end
+
   def sign_in_params
     devise_parameter_sanitizer.sanitize(:sign_in)
   end
