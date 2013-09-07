3019828d784486d843184770969ce2aad37e4d22
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-09-07 13:49:17 -0700

    instantiate logincontroller inside signupdialogcontroller, this is the win

--- webapp/app/assets/javascripts/controllers/sign_up_dialog_controller.js
+++ webapp/app/assets/javascripts/controllers/sign_up_dialog_controller.js
@@ -1,6 +1,5 @@
 angular.module("app.controllers")
 .controller('SignUpDialogController', ['$scope', 'fs', '$dialog', function($scope, fs, $dialog) {
-  $scope.login = login;
   $scope.user = $scope.user || {};
   $scope.signInForm = false;
   $scope.signUp = function() {
@@ -12,23 +11,6 @@ angular.module("app.controllers")
 
   $scope.toggleSignInForm = function(){
     $scope.signInForm = !$scope.signInForm;
-  };
-
-  var serviceSizes = {
-    facebook: 'height=460,width=730',
-    /*  linkedin: 'height=260,width=630', // customize these
-      google: 'height=260,width=630'*/
-  };
-
-  $scope.login = function(service) {
-    if(service === 'email'){
-      fs.user.login($scope.user).then(function(resp){
-        // window.setCurrentUser(resp);
-        window.location.reload(true);
-      });
-    } else {
-      window.open('/users/auth/' + service, '', serviceSizes[service]);
-    }
   };
 
 }]);
