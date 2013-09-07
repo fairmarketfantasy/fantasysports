107cdb9778bac7dbf2f5f4452e32e4ff38f07d02
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-09-07 13:32:57 -0700

    working out the login modal
    * some scope issues with login function.
    * the dialog doesn't inherit the scope of application controller.
    * I could use `resolve` to pass the login function into the scope of
    the modal controller, but then when the login function fires $scope.user
    is undefined, which gets awkward,
    * not sure the best way to handle this yet

--- webapp/app/assets/javascripts/controllers/login_controller.js
+++ webapp/app/assets/javascripts/controllers/login_controller.js
@@ -1,11 +1,6 @@
 angular.module("app.controllers")
-.controller('LoginController', ['$scope', 'fs', '$dialog', function($scope, fs, $dialog) {
-  // setup fb signup buttons
-  var serviceSizes = {
-    facebook: 'height=460,width=730',
-  /*  linkedin: 'height=260,width=630', // customize these
-    google: 'height=260,width=630'*/
-  };
+.controller('LoginController', ['$scope', '$dialog', function($scope, $dialog) {
+
   $scope.signUpModal = function(){
     var dialogOpts = {
           backdrop: true,
@@ -13,13 +8,19 @@ angular.module("app.controllers")
           backdropClick: true,
           dialogClass: 'modal',
           templateUrl: '/assets/sign_up_dialog.html',
-          controller: 'SignUpDialogController'
+          controller: 'SignUpDialogController',
         };
 
      var d = $dialog.dialog(dialogOpts);
      d.open();
   };
-  $scope.user = $scope.user || {};
+
+  var serviceSizes = {
+    facebook: 'height=460,width=730',
+    /*  linkedin: 'height=260,width=630', // customize these
+      google: 'height=260,width=630'*/
+  };
+
   $scope.login = function(service) {
     if(service === 'email'){
       fs.user.login($scope.user).then(function(resp){
@@ -30,5 +31,6 @@ angular.module("app.controllers")
       window.open('/users/auth/' + service, '', serviceSizes[service]);
     }
   };
+
 }]);
 
--- webapp/app/assets/javascripts/controllers/sign_up_dialog_controller.js
+++ webapp/app/assets/javascripts/controllers/sign_up_dialog_controller.js
@@ -1,15 +1,34 @@
 angular.module("app.controllers")
 .controller('SignUpDialogController', ['$scope', 'fs', '$dialog', function($scope, fs, $dialog) {
+  $scope.login = login;
   $scope.user = $scope.user || {};
   $scope.signInForm = false;
   $scope.signUp = function() {
     fs.user.create($scope.user).then(function(resp){
-      console.log(resp);
+      // console.log(resp);
+      window.location.reload(true);
     });
   };
 
   $scope.toggleSignInForm = function(){
     $scope.signInForm = !$scope.signInForm;
+  };
+
+  var serviceSizes = {
+    facebook: 'height=460,width=730',
+    /*  linkedin: 'height=260,width=630', // customize these
+      google: 'height=260,width=630'*/
+  };
+
+  $scope.login = function(service) {
+    if(service === 'email'){
+      fs.user.login($scope.user).then(function(resp){
+        // window.setCurrentUser(resp);
+        window.location.reload(true);
+      });
+    } else {
+      window.open('/users/auth/' + service, '', serviceSizes[service]);
+    }
   };
 
 }]);
