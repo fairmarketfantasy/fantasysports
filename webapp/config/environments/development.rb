62224c880f07d8e204f92ef03ec2709623899909
Mike Ihbe <mihbe@yammer-inc.com>
2013-09-14 01:36:22 -0700

    Still can't figure out how to add digests to html files in dev, but i've figured it out in prod

--- webapp/config/environments/development.rb
+++ webapp/config/environments/development.rb
@@ -39,4 +39,5 @@ Fantasysports::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.assets.digest = true
 end
