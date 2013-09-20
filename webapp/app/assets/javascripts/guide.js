f138db7fc72d159b7768a1e56363633ffb2c5102
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-09-20 13:24:54 -0700

    get guide set up with angular app guide

--- webapp/app/assets/javascripts/guide.js
+++ webapp/app/assets/javascripts/guide.js
@@ -1,1 +1,15 @@
+//= require jquery-2.0.3.min.js
+//= require underscore-min.js
+//= require angular-1.0.7.min.js
 //= require angular-ui-bootstrap-tpls-0.4.0.min.js
+//= require_self
+
+angular.module('guide', ['guide.controllers', 'ui.bootstrap']);
+angular.module('guide.controllers', [])
+.controller('ApplicationController', ['$scope', function($scope) {
+  $scope.items = [
+     "Choice 1",
+     "Choice 2",
+     "Choice 3"
+    ];
+}]);
