0c26fa592ddff0d2d23408252c753fbd5bd9b0a3
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-09-23 03:16:19 -0700

    Modals use flash messages #35, test for card creation and deletion
    * enhancement to mustwin-stripe-ruby-mock to support credit cards
    * improve test coverage of adding and deleting cards
    * all modals now make use of flash, much nicer
    * fix some bugs around saving CC md5 hash

--- webapp/test/controllers/cards_controller_test.rb
+++ webapp/test/controllers/cards_controller_test.rb
@@ -6,26 +6,24 @@ class CardsControllerTest < ActionController::TestCase
     user = create(:user) #doesn't have a customer object
     sign_in(user)
     assert_difference("CustomerObject.count", 1) do
-      xhr :post, :create, {token: valid_card_token}
-      pp response.body
+      assert_difference("CreditCard.count", 1) do
+        xhr :post, :create, {token: valid_card_token, card_number: '4242-4242-4242-4242'}
+      end
     end
   end
 
-
-## TODO- tests for this... in order to make the bottom two tests work,
-## the stripe-ruby-mock gem needs to support requets to retrieve cards
-
-  # test "post to create when a user already has a customer object" do
-  #   user = create(:paid_user) #already has a customer object
-  #   sign_in(user)
-  #   assert_difference("CustomerObject.count", 1) do
-  #     xhr :post, :create, {token: valid_card_token}
-  #   end
-  # end
+  test "post to create when a user already has a customer object" do
+    user = create(:paid_user) #already has a customer object
+    sign_in(user)
+    assert_no_difference("CustomerObject.count") do
+      assert_difference("CreditCard.count", 1) do
+        xhr :post, :create, {token: valid_card_token, card_number: '4242-4242-4242-4242' }
+      end
+    end
+  end
 
   # test "delete to /cards/:id" do
   #   user = create(:paid_user)
-  #   binding.pry
   #   card_id = user.customer_object.default_card_id
   #   sign_in(user)
   #   user.customer_object.expects(:delete_a_card).with(card_id)
