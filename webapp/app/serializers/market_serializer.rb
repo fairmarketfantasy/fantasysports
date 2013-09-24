b6f882a1411253d0d8391817cd59012cab0bfb6e
Mike Ihbe <mihbe@yammer-inc.com>
2013-09-24 11:25:44 -0700

    Add in icon urls and market names

--- webapp/app/serializers/market_serializer.rb
+++ webapp/app/serializers/market_serializer.rb
@@ -1,6 +1,14 @@
 class MarketSerializer < ActiveModel::Serializer
   attributes :id, :name, :shadow_bets, :shadow_bet_rate, :started_at, :opened_at, :closed_at, :sport_id, :total_bets, :state, :market_duration
 
+  def name
+    if object.name.blank?
+      "One Day"
+    else
+      object.name
+    end
+  end
+
   def market_duration
     if object.closed_at && object.started_at
       if (object.closed_at > object.started_at + 1.day)
