8df6acd4be465c5c47851dee15520f1ac0f6276b
Mike Ihbe <mihbe@yammer-inc.com>
2013-09-24 12:56:59 -0700

    Add user stats

--- webapp/app/serializers/user_serializer.rb
+++ webapp/app/serializers/user_serializer.rb
@@ -1,10 +1,14 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :admin, :email, :balance, :image_url
+  attributes :id, :name, :admin, :email, :balance, :image_url, :win_percentile, :total_points, :total_wins, :joined_at
   has_one :in_progress_roster
 
   attribute :confirmed?, key: :confirmed
 
   def balance
     object.customer_object.try(:balance) || 0
+  end
+
+  def joined_at
+    object.created_at
   end
 end
