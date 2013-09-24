1a7d84bfd555d6393a39fd6d1448aad3bec7aeb7
Mike Ihbe <mihbe@yammer-inc.com>
2013-09-24 11:25:44 -0700

    Add in icon urls and market names

--- webapp/app/serializers/contest_type_serializer.rb
+++ webapp/app/serializers/contest_type_serializer.rb
@@ -1,4 +1,8 @@
 class ContestTypeSerializer < ActiveModel::Serializer
-  attributes :id, :market_id, :name, :description, :max_entries, :buy_in, :rake, :payout_structure, :user_id, :private, :payout_description
+  attributes :id, :market_id, :name, :description, :max_entries, :buy_in, :rake, :payout_structure, :user_id, :private, :payout_description, :icon_url
+
+  def icon_url
+    "/assets/icon-#{name}.png"
+  end
 end
 
