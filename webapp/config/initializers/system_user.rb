2eca44272a5c867cf794249aafe840a4c6043d54
Mike Ihbe <mihbe@yammer-inc.com>
2013-09-25 01:39:22 -0700

    fix reload

--- webapp/config/initializers/system_user.rb
+++ webapp/config/initializers/system_user.rb
@@ -1,3 +1,7 @@
-SYSTEM_USER = User.find_by_email "fantasysports@mustw.in"
+SYSTEM_USER = begin
+  User.find_by_email "fantasysports@mustw.in" 
+rescue 
+  nil
+end
 Rails.logger.warn "=" * 50 + "\nSystem user not installed. Run `rake db:seed`\n" + "=" * 50 unless SYSTEM_USER
 
