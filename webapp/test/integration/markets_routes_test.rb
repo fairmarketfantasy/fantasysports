fefb8e41c95f015afb024ed4dd4ed3748c50bcbc
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-09-25 02:37:06 -0700

    remove no longer relavant route tests for markets/:id/contests

--- webapp/test/integration/markets_routes_test.rb
+++ webapp/test/integration/markets_routes_test.rb
@@ -10,14 +10,4 @@ class MarketRoutesTest < ActionDispatch::IntegrationTest
     assert_routing({path: "/markets", method: :get}, {controller: "markets", action: "index"})
   end
 
-  test "get markets contests" do
-    m = @market
-    assert_routing({path: "/markets/#{m.id}/contests", method: :get}, {controller: "markets", id: m.id.to_s, action: "contests"})
-  end
-
-  test "post markets contests" do
-    m = @market
-    assert_routing({path: "/markets/#{m.id}/contests", method: :post}, {controller: "markets", id: m.id.to_s, action: "contests"})
-  end
-
 end
