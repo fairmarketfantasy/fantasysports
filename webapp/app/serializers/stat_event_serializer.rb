ae2a21464d6593e00056b068dbde5679a6eeb7a4
Mike Ihbe <mihbe@yammer-inc.com>
2013-09-26 18:00:31 -0700

    Fix highlight on change, puchase price display, stat activities, etc

--- webapp/app/serializers/stat_event_serializer.rb
+++ webapp/app/serializers/stat_event_serializer.rb
@@ -1,4 +1,4 @@
 class StatEventSerializer < ActiveModel::Serializer
-  attributes :id, :type, :data, :point_value, :player_stats_id, :game_stats_id
+  attributes :id, :activity, :data, :point_value, :player_stats_id, :game_stats_id
 end
 
