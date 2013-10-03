139df2cd6f92658534cac907cf6457101a75b2e9
Mike Ihbe <mihbe@yammer-inc.com>
2013-10-03 08:36:15 -0400

    Add carrierwave requires

--- webapp/config/application.rb
+++ webapp/config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require 'devise_oauth2_providable'
+require 'carrierwave'
+require 'carrierwave/orm/activerecord'
 
 # Setup api formats.
 ActiveSupport.on_load(:active_model_serializers) do
