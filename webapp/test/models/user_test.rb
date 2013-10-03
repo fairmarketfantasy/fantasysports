399df5ea998b51b8bd6f1723373b659a5ae82f13
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-10-03 08:48:20 -0700

    test for avatar uploader and gitignore for public/uploads

--- webapp/test/models/user_test.rb
+++ webapp/test/models/user_test.rb
@@ -14,15 +14,15 @@ class UserTest < ActiveSupport::TestCase
 
     end
 
-    # describe "avatar uploader" do
+    describe "avatar uploader" do
 
-    #   let(:file) { fixture_file_upload('mickey.png', 'image/png') }
+      let(:file) { fixture_file_upload('mickey.png', 'image/png') }
 
-    #   it "should be able to take an upload" do
-    #     user.avatar = file
-    #     user.save!
-    #     user.avatar.url.should_not be_nil
-    #   end
-    # end
+      it "should be able to take an upload" do
+        user.avatar = file
+        user.save!
+        user.avatar.url.wont_be_nil
+      end
+    end
   end
 end
