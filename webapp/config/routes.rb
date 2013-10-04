6b311ddefdd4235283e46c6e592ebaef0907298b
Mike Ihbe <mihbe@yammer-inc.com>
2013-10-04 04:17:21 -0400

    Remove rails admin

--- webapp/config/routes.rb
+++ webapp/config/routes.rb
@@ -9,7 +9,6 @@ Fantasysports::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   Rails.application.routes.draw do
-    mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
     # oauth routes can be mounted to any path (ex: /oauth2 or /oauth)
     mount Devise::Oauth2Providable::Engine => '/oauth2'
   end
