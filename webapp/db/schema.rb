9871f35f7f2d3c3401fb68ea5a59f03f1f39bf3f
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-10-11 14:47:36 -0700

    masters Gemfile.lock and db/schema

--- webapp/db/schema.rb
+++ webapp/db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20131001143528) do
-=======
 ActiveRecord::Schema.define(version: 20131010231143) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -402,13 +398,10 @@ ActiveRecord::Schema.define(version: 20131010231143) do
     t.integer  "total_wins",             default: 0,     null: false
     t.decimal  "win_percentile",         default: 0.0,   null: false
     t.integer  "token_balance",          default: 0
-<<<<<<< HEAD
-    t.string   "avatar"
-=======
     t.string   "username"
     t.string   "fb_token"
     t.integer  "inviter_id"
->>>>>>> master
+    t.string   "avatar"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
