2648fcc4d7368304d745ecb521e51c73c1160969
Mike Ihbe <mihbe@yammer-inc.com>
2013-10-11 14:35:15 +0200

    Some deploy things

--- webapp/config/deploy/god_web.rb
+++ webapp/config/deploy/god_web.rb
@@ -5,7 +5,7 @@ God.pid_file_directory = PID_PATH
 
 God.watch do |w|
   w.name = "puma"
-  w.start = "bundle exec puma -t 0:16 -w 2 -e #{ENV['RAILS_ENV']} -b unix://#{BASE_DIR}/shared/tmp/puma.sock --pidfile #{PID_PATH}/puma.pid"
+  w.start = "bundle exec puma -t 0:16 -w 2 -e #{ENV['RAILS_ENV']} -b unix://0.0.0.0:80 --pidfile #{PID_PATH}/puma.pid"
   w.dir = BASE_DIR + '/current/webapp'
   w.log = BASE_DIR + '/shared/log/puma.log'
   w.env = {"RAILS_ENV" => ENV['RAILS_ENV']}
