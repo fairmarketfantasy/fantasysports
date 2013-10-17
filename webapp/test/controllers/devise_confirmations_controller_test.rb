cf5d917dd99dd7aeb68d93c939e24c0a09961fac
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-10-17 12:11:31 -0700

    devise confirmations show with the token signs in and refreshed the page

--- webapp/test/controllers/devise_confirmations_controller_test.rb
+++ webapp/test/controllers/devise_confirmations_controller_test.rb
@@ -13,4 +13,18 @@ class Users::ConfirmationsControllerTest < ActionController::TestCase
       xhr :post, :create
     end
   end
+
+  test "get to users/confirm with token" do
+    user = create(:paid_user)
+    raw, enc = Devise.token_generator.generate(user.class, :confirmation_token)
+    user.confirmation_token   = enc
+    user.confirmation_sent_at = Time.now.utc
+    user.confirmed_at = nil
+    user.save!
+
+    xhr :get, :show, confirmation_token: raw
+    user.reload
+    assert user.confirmed?
+    assert_equal @controller.current_user, user, "user is signed in"
+  end
 end
