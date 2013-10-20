c01feec94da1548723a75a065d4597b66a6f66d3
Mike Ihbe <mihbe@yammer-inc.com>
2013-10-20 15:03:04 +0200

    reenable the poller

--- webapp/app/assets/javascripts/services/rosters.js
+++ webapp/app/assets/javascripts/services/rosters.js
@@ -218,7 +218,7 @@ angular.module('app.data')
 
       this.setPoller = function(fn, interval) {
         clearInterval(this.poller);
-        //this.poller = setInterval(fn, interval);
+        this.poller = setInterval(fn, interval);
       };
     }();
   }])
