89f08fb477223dd371fa2224d074988a6725657a
Mike Ihbe <mihbe@yammer-inc.com>
2013-10-21 19:34:37 +0200

    Fix market buy_in_ratio

--- market/market.sql
+++ market/market.sql
@@ -11,7 +11,7 @@
 DROP FUNCTION buy_in_ratio(boolean);
 
 CREATE OR REPLACE FUNCTION buy_in_ratio(_takes_tokens boolean) RETURNS numeric AS $$
-  SELECT CASE WHEN $1=false THEN 1 ELSE 1 END;
+  SELECT CASE WHEN $1=false THEN 1.0 ELSE 1.0 END;
 $$ LANGUAGE SQL IMMUTABLE;
 
 -- /* The pricing function. Right now, it's a straight linear shot and assumes a 100k salary cap with a 1k minimum price */
