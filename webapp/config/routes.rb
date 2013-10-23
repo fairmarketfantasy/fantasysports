5d214a31c87bf3df5ba3814e64fb663ee798ea81
Mike Ihbe <mihbe@yammer-inc.com>
2013-10-24 00:45:32 +0200

    Add ssl configs

--- webapp/config/routes.rb
+++ webapp/config/routes.rb
@@ -21,6 +21,7 @@ Fantasysports::Application.routes.draw do
   end
 
   # You can have the root of your site routed with "root"
+  get '/healthcheck' => 'application#healthcheck'
   post '/support' => 'pages#support'
   get '/terms' => 'pages#terms'
   get '/guide' => 'pages#guide'
