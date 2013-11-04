014fe56649ccc0ab3040982508416acbbb7eca45
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-11-04 01:09:45 -0800

    reset password flow is working, refactor sign_in_dialog to have three states: signInForm, signUpForm and resetPass

--- webapp/app/assets/javascripts/controllers/sign_up_dialog_controller.js
+++ webapp/app/assets/javascripts/controllers/sign_up_dialog_controller.js
@@ -2,17 +2,40 @@ angular.module("app.controllers")
 .controller('SignUpDialogController', ['$scope', 'dialog', 'flash', 'fs', 'message', function($scope, dialog, flash, fs, message) {
   $scope.user = $scope.user || {};
   $scope.message = message;
+
+  // ['forgotPass', 'signUpForm', 'signInForm']
+  $scope.changeState = function(state){
+
+    //set all states to false
+    states =['forgotPass', 'signUpForm', 'signInForm'];
+    _.each(states, function(st){
+      $scope[st] = false;
+    });
+
+    $scope[state] = true;
+
+    if(state === 'forgotPass'){
+      $scope.title = "Forgot Password";
+      $scope.message = 'Enter your email address for instructions';
+    } else if(state === 'signUpForm') {
+      $scope.title = "Sign Up";
+      $scope.message = '';
+    } else if(state === 'signInForm') {
+      $scope.title = "Sign In";
+      $scope.message = '';
+    }
+  };
+
   if (message == 'signin') {
-    $scope.signInForm = true;
-    $scope.message = '';
+    $scope.changeState('signInForm');
   } else {
-    $scope.signInForm = false;
+    $scope.changeState('signUpForm');
   }
 
   $scope.signUp = function() {
     fs.user.create($scope.user).then(function(resp){
       //only fires on success, errors are intercepted by fsAPIInterceptor
-      window.location.reload(true);
+      window.location.href = '/';
     });
   };
 
@@ -23,14 +46,6 @@ angular.module("app.controllers")
       flash.success = resp.message;
       $scope.close();
     });
-  };
-
-  $scope.showForgotPass = function(){
-    $scope.forgotPass = true;
-  };
-
-  $scope.toggleSignInForm = function(){
-    $scope.signInForm = !$scope.signInForm;
   };
 
   $scope.isValid = function(){
