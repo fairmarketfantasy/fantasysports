2979ed7409fc7028c2eb3ad933ccd181921eacd5
Mike Ihbe <mihbe@yammer-inc.com>
2013-11-06 01:35:26 -0800

    Add existing users to leagues when sent invitations

--- webapp/app/models/invitation.rb
+++ webapp/app/models/invitation.rb
@@ -26,6 +26,9 @@ class Invitation < ActiveRecord::Base
         contest_type_id: contest.private? ? nil : contest.contest_type_id,
         code: SecureRandom.hex(16)
       )
+      if contest.league? && (invited = User.where(:email => email).first)
+       LeagueMembership.where(:league_id => contest.league.id, :user_id => invited.id).find_or_create
+      end
     end
     ContestMailer.invite_to_contest(invitation, inviter, contest, email, message).deliver!
     invitation
