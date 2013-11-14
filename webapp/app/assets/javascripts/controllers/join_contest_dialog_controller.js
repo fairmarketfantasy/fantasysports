84158c3481cd9da6eb0da6f5c0eef9085480b5b6
Ben Atkin <ben@benatkin.com>
2013-11-14 01:46:32 -0700

    don't include the weekly contest types

--- webapp/app/assets/javascripts/controllers/join_contest_dialog_controller.js
+++ webapp/app/assets/javascripts/controllers/join_contest_dialog_controller.js
@@ -1,14 +1,13 @@
 angular.module("app.controllers")
 .controller('JoinContestDialogController', ['$scope', 'dialog', 'fs', 'flash', 'markets', 'currentUserService', '$timeout', 'contestClasses', function($scope, dialog, fs, flash, marketService, currentUserService, $timeout, contestClasses) {
 
-  // [start] from MarketController.
-  // TODO: roll into marketService
-  $scope.contestTypeOrder = ['100k', '10k', '5k', '194', '970', 'h2h', 'h2h rr'];
+  // Don't include 100k, 10k, 5k contests from MarketController since these are periodic (weekly)
+  // and unlikely to be signed up for immediately after submitting a roster.
+  $scope.contestTypeOrder = ['194', '970', 'h2h', 'h2h rr'];
 
   $scope.isBigContest = function(contestClass) {
     return !!contestClass.match(/\d+k/);
   };
-  // [end]
 
   $scope.contestClasses = contestClasses;
 
@@ -19,7 +18,6 @@ angular.module("app.controllers")
   };
 
   $scope.close = function(result){
-    console.log('contestClasses', contestClasses);
     dialog.close(result);
   };
 }]);
