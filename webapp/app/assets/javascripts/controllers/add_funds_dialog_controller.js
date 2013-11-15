25c43a58ba8dd78dbc7038422a6c4ddec135a399
Mike Ihbe <mihbe@yammer-inc.com>
2013-11-14 17:30:34 -0800

    REVERT THIS TO TURN ON CREDIT CARDS

--- webapp/app/assets/javascripts/controllers/add_funds_dialog_controller.js
+++ webapp/app/assets/javascripts/controllers/add_funds_dialog_controller.js
@@ -5,7 +5,8 @@ angular.module("app.controllers")
    $scope.currentUser = currentUserService.currentUser;
    $scope.cardInfo    = $scope.cardInfo     || {};
    $scope.cards       = $scope.cards        || [];
-  $scope.payment_type = 'credit-card';
+  $scope.payment_type = 'paypal';
+  $scope.showCardForm = false;
 
   $scope.close = function(){
     dialog.close();
@@ -16,11 +17,11 @@ angular.module("app.controllers")
    fs.cards.list().then(function(resp){
      $scope.cards = resp.cards || [];
      if(!$scope.cards.length){
-       flash.error("You don't have any cards, add one.");
+       //flash.error("You don't have any cards, add one.");
      } else {
        $scope.focusAmount = true;
      }
-     $scope.showCardForm = !$scope.cards.length;
+     $scope.showCardForm = false;//!$scope.cards.length;
      $scope.loaded = true;
      setSelectedCardId();
    });
