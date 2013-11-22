ef1e532da735113ad97734b3ed32105b751fe799
Mike Ihbe <mihbe@yammer-inc.com>
2013-11-21 20:04:59 -0800

    Add benching and unbenching players to the player admin

--- webapp/db/schema.rb
+++ webapp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131123214328) do
+ActiveRecord::Schema.define(version: 20131125214538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -345,6 +345,7 @@ ActiveRecord::Schema.define(version: 20131123214328) do
     t.boolean  "is_generated",     default: false
     t.integer  "bonus_points",     default: 0
     t.text     "bonuses"
+    t.string   "view_code"
   end
 
   add_index "rosters", ["contest_id"], name: "index_rosters_on_contest_id", using: :btree
@@ -448,10 +449,10 @@ ActiveRecord::Schema.define(version: 20131123214328) do
     t.integer  "total_wins",             default: 0,     null: false
     t.decimal  "win_percentile",         default: 0.0,   null: false
     t.integer  "token_balance",          default: 0
-    t.string   "avatar"
     t.string   "username"
     t.string   "fb_token"
     t.integer  "inviter_id"
+    t.string   "avatar"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
