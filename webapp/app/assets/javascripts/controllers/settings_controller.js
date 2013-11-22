ea0794cd1b6ff97d130befdaae50b0354e090eca
Ben Atkin <ben@benatkin.com>
2013-11-22 02:16:55 -0700

    use proper endpoint, keep currentUser and userInfo and in sync where appropriate

--- webapp/app/assets/javascripts/controllers/settings_controller.js
+++ webapp/app/assets/javascripts/controllers/settings_controller.js
@@ -2,7 +2,13 @@
 .controller('SettingsController', ['$scope', '$timeout', 'dialog', 'fs', 'flash', 'currentUserService', function($scope, $timeout, dialog, fs, flash, currentUserService) {
 
   $scope.currentUserService = currentUserService;
-  $scope.currentUser = currentUserService.currentUser;
+
+  $scope.setUserInfo = function() {
+    $scope.currentUser = currentUserService.currentUser;
+    $scope.userInfo = $scope.userInfo || {id: $scope.currentUser.id, name: $scope.currentUser.name, username: $scope.currentUser.username, email: $scope.currentUser.email};
+  }
+
+  $scope.setUserInfo();
 
   var flashForConfirm = function(){
     if(!$scope.currentUser.confirmed){
@@ -14,20 +20,26 @@
 
   flashForConfirm();
 
-  $scope.userInfo = $scope.userInfo || {id: $scope.currentUser.id, name: $scope.currentUser.name, email: $scope.currentUser.email};
+  $scope.editUser = function() {
+    $scope.setUserInfo();
+    $scope.showUserForm = true;
+  }
 
   $scope.isAuthedWithFacebook = !!$scope.currentUser.provider;
 
-//fires after multipart upload for picture has completed
+  //fires after multipart upload for picture has completed
   $scope.results = function(content, completed) {
     if (completed)
-      fs.user.refresh().then(function(resp){
-        $scope.currentUser = resp;
-        window.App.currentUser = resp;
+      // the response includes the updated user, but it's simpler to get it straight from $http rather than ng-upload
+      fs.user.refresh().then(function(data){
+        flash.success("Success, avatar saved");
+        currentUserService.setUser(data);
+        $scope.setUserInfo();
         $scope.showUpload  = false;
       });
     else
     {
+      flash.error("Error saving avatar");
       // 1. ignore content and adjust your model to show/hide UI snippets; or
       // 2. show content as an _operation progress_ information
     }
@@ -41,11 +53,15 @@
   };
 
   $scope.updateUser = function(){
-    fs.user.update($scope.userInfo).then(function(resp){
+    fs.user.update($scope.userInfo).then(function(data){
       flash.success("Success, user info saved");
       flashForConfirm();
-      $scope.currentUser = resp;
-      $scope.userInfo = {};
+      currentUserService.setUser(data);
+      $scope.setUserInfo();
+      $scope.showUserForm = false;
+      $scope.showPasswordForm = false;
+    }, function() {
+      flash.error("Error saving user info");
     });
   };
 
