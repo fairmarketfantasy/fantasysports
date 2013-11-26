49546abf272f40a86cbb3a871f2e38817c6a5ca4
Ben Atkin <ben@benatkin.com>
2013-11-26 02:00:58 -0700

    use angular for validation

--- webapp/app/assets/javascripts/controllers/forgot_password_controller.js
+++ webapp/app/assets/javascripts/controllers/forgot_password_controller.js
@@ -2,12 +2,25 @@ angular.module("app.controllers")
 .controller('ForgotPasswordDialogController', ['$scope', 'dialog', 'flash', 'fs', '$timeout', 'registrationService', function($scope, dialog, flash, fs, $timeout, registrationService) {
   $scope.user = $scope.user || {};
 
-  $scope.resetPassword = function(){
+  $scope.submit = function() {
+    if (! $scope.isValid()) return;
     fs.user.resetPassword($scope.user.email).then(function(resp){
       flash.success(resp.message);
       $scope.close();
     });
+  }
+
+  $scope.isValid = function(){
+    var required       = ($scope.user.email);
+
+    $scope.errorMsg = null;
+    if (!required) {
+      $scope.errorMsg = "All fields are required";
+    }
+    return !$scope.errorMsg;
   };
+
+  $scope.login = registrationService.login;
 
   $scope.close = function(nextModal){
     dialog.close();
--- webapp/app/assets/javascripts/controllers/login_dialog_controller.js
+++ webapp/app/assets/javascripts/controllers/login_dialog_controller.js
@@ -5,6 +5,28 @@ angular.module("app.controllers")
   $scope.forgotPass = false;
 
   // Uses login() method from LoginController
+  
+  $scope.submit = function() {
+    if (! $scope.isValid()) return;
+    fs.user.login($scope.user).then(function(resp){
+      // window.setCurrentUser(resp);
+      window.location.reload(true);
+    }, function() {
+      $scope.errorMsg = 'There was an error signing you in. Please try again. Use the link below if you need to reset your password.';
+    });
+  }
+
+  $scope.isValid = function(){
+    var required       = ($scope.user.email && $scope.user.password);
+
+    $scope.errorMsg = null;
+    if (!required) {
+      $scope.errorMsg = "All fields are required";
+    }
+    return !$scope.errorMsg;
+  };
+
+  $scope.login = registrationService.login;
 
   $scope.close = function(nextModal){
     dialog.close();
