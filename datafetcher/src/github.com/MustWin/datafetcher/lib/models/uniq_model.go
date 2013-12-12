c786f91eef2f07a95d1d29e6893c2e8703cdc92c
Mike Ihbe <mihbe@yammer-inc.com>
2013-12-11 21:00:26 -0800

    Add json support for structs, saving game state

--- datafetcher/src/github.com/MustWin/datafetcher/lib/models/uniq_model.go
+++ datafetcher/src/github.com/MustWin/datafetcher/lib/models/uniq_model.go
@@ -3,60 +3,18 @@ package models
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/MustWin/datafetcher/lib"
 	"github.com/MustWin/datafetcher/lib/model"
+	"github.com/MustWin/datafetcher/lib/utils"
 	"log"
 	"reflect"
 	"strconv"
 	"time"
 )
 
-func z() { log.Println("HERE") }
-
-type NflOrm struct {
-	model.OrmBase
-}
-
-type NflModel struct {
-	model.ModelBase
-}
-
-// Todo: move these to lib.types once we refactor the "unique model" functionality
-type Market struct {
-	NflModel
-	Id              int
-	Name            string
-	SportId         int `model:"key"`
-	TotalBets       float64
-	ShadowBets      float64
-	ShadowBetRate   float64
-	PublishedAt     time.Time
-	State           string
-	StartedAt       time.Time `model:"key"`
-	FillRosterTimes string
-	OpenedAt        time.Time
-	ClosedAt        time.Time `model:"key"`
-	CreatedAt       time.Time
-	UpdatedAt       time.Time
-}
-
-type MarketPlayer struct {
-	NflModel
-	Id       int
-	MarketId int `model:"key"`
-	PlayerId int `model:"key"`
-}
-
-type GamesMarket struct {
-	NflModel
-	Id          int
-	MarketId    int    `model:"key"`
-	GameStatsId string `model:"key"`
-}
-
+/* Some utilities */
 func supportsJsonEncoding(val reflect.Value) bool {
 	// Find all key tags and build query to find unique object
-	valType := reflect.TypeOf(val).Elem()
+	valType := reflect.TypeOf(val.Interface())
 	for i := 0; i < valType.NumField(); i++ {
 		field := valType.Field(i)
 		tag := field.Tag
@@ -126,8 +84,12 @@ func valueToString(val reflect.Value) string {
 		if val.FieldByName("sec").IsValid() && val.FieldByName("loc").IsValid() {
 			realValue := val.Interface().(time.Time)
 			return realValue.Format("Mon Jan 2 15:04:05 -0700 MST 2006")
-		} else if string(supportsJsonEncoding(val)) {
-			return json.Marshal(val.Interface())
+		} else if supportsJsonEncoding(val) {
+			json, err := json.Marshal(val.Interface())
+			if err != nil {
+				log.Panicf("Unable to convert value to json string %s", val.Interface())
+			}
+			return string(json)
 		} else {
 			log.Panicf("Unable to convert value to string %s", val.Interface())
 		}
@@ -144,8 +106,13 @@ func valueToString(val reflect.Value) string {
 	return ""
 }
 
+/* Definition */
+type UniqModel struct {
+	model.ModelBase
+}
+
 // This basically arranges things so that this updates changed attributes of unique objects, and only saves them if they didn't exist to begin with
-func (n *NflModel) BeforeSave(db model.Orm, m model.Model) (error, bool) {
+func (n *UniqModel) BeforeSave(db model.Orm, m model.Model) (error, bool) {
 	val := reflect.Indirect(reflect.ValueOf(m))
 	valType := reflect.TypeOf(m).Elem()
 	orm := db.GetDb()
@@ -155,7 +122,7 @@ func (n *NflModel) BeforeSave(db model.Orm, m model.Model) (error, bool) {
 		field := valType.Field(i)
 		tag := field.Tag
 		if tag.Get("model") == "key" || reflect.ValueOf(tag).String() == "key" {
-			args[lib.SnakeCase(field.Name)] = val.FieldByIndex(field.Index).Interface()
+			args[utils.SnakeCase(field.Name)] = val.FieldByIndex(field.Index).Interface()
 			//orm = orm.Where(fmt.Sprintf("%s = $1", lib.SnakeCase(field.Name)), val.FieldByIndex(field.Index).Interface())
 		}
 	}
@@ -181,6 +148,7 @@ func (n *NflModel) BeforeSave(db model.Orm, m model.Model) (error, bool) {
 		if err.Error() == "No record found" {
 			return nil, true
 		} else {
+			log.Panic(err.Error())
 			log.Println(err.Error())
 		}
 	}
@@ -189,7 +157,7 @@ func (n *NflModel) BeforeSave(db model.Orm, m model.Model) (error, bool) {
 	// Set non-zero fields
 	for i := 0; i < valType.NumField(); i++ {
 		field := valType.Field(i)
-		if field.Type.Kind().String() == "struct" && !val.FieldByName(field.Name).FieldByName("sec").IsValid() {
+		if field.Type.Kind().String() == "struct" && !(val.FieldByName(field.Name).FieldByName("sec").IsValid() || supportsJsonEncoding(val.FieldByName(field.Name))) {
 			continue
 		}
 		newFieldVal := val.FieldByName(field.Name)
@@ -208,168 +176,4 @@ func (n *NflModel) BeforeSave(db model.Orm, m model.Model) (error, bool) {
 	}
 	err = orm.Save(existing)
 	return err, false
-}
-
-type Team struct {
-	NflModel
-	Id         int
-	SportId    int    `model:"key"`
-	Abbrev     string `model:"key"`
-	Name       string
-	Conference string
-	Division   string
-	Market     string
-	Country    string
-	Lat        float64
-	Long       float64
-	Standings  string
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
-}
-
-type Weather struct {
-	Temperature string
-	condition   string
-	Humidity    string
-}
-
-type Venue struct {
-	NflModel
-	Id      int
-	StatsId string `model:"key"`
-	Country string
-	State   string
-	City    string
-	Type    string
-	Name    string
-	Surface string
-	Weather Weather
-}
-
-type TeamStatus struct {
-	Points              int `json:"points"`
-	RemainingTimeouts   int `json:"remainingTimeout"`
-	RemainingChallenges int `json:"remainingChallenges"`
-}
-
-type Game struct {
-	NflModel
-	Id             int
-	StatsId        string `model:"key"`
-	SeasonType     string
-	SeasonYear     int
-	SeasonWeek     int
-	HomeTeam       string
-	AwayTeam       string
-	HomeTeamStatus TeamStatus
-	AwayTeamStatus TeamStatus
-	GameDay        time.Time
-	GameTime       time.Time
-	Status         string
-	Venue          Venue
-	Network        string
-	CreatedAt      time.Time
-	UpdatedAt      time.Time
-	BenchCountedAt time.Time
-}
-
-type PlayerStatus struct {
-	Description    string
-	StartDate      time.Time
-	GameStatus     string
-	PracticeStatus string
-}
-
-type Player struct {
-	NflModel
-	Id           int
-	StatsId      string `model:"key"`
-	SportId      int
-	Team         string
-	Name         string
-	NameAbbr     string
-	Birthdate    string
-	Height       int
-	Weight       int
-	College      string
-	Position     string
-	JerseyNumber int
-	Status       string
-	PlayerStatus PlayerStatus
-	TotalGames   int
-	TotalPoints  int
-	//BenchedGames int
-	CreatedAt time.Time
-	UpdatedAt time.Time
-}
-
-type StatEvent struct {
-	NflModel
-	Id            int
-	GameStatsId   string `model:"key"`
-	PlayerStatsId string `model:"key"`
-	Activity      string `model:"key"`
-	Data          string
-	//  PointType string
-	PointValue float64
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
-}
-
-// Used by defenseParser
-func (se *StatEvent) AddOpposingTeamScore(score int) {
-	// opponent score pts
-	// 0: 10pts
-	// 2-6: 7pts
-	// 7-13: 4pts
-	// 14-17: 1pts
-	// 22-27: -1pts
-	// 28-34: -4pts
-	// 35-45: -7pts
-	// >46: -10pts
-	pts := 0
-	switch {
-	case score == 0:
-		pts = 10
-	case score <= 6:
-		pts = 7
-	case score <= 13:
-		pts = 4
-	case score <= 17:
-		pts = 1
-	case score <= 22:
-		pts = 0
-	case score <= 27:
-		pts = -1
-	case score <= 34:
-		pts = -4
-	case score <= 45:
-		pts = -7
-	case score > 45:
-		pts = -10
-	}
-	se.PointValue += float64(pts)
-}
-
-type GameEventData struct {
-	Side     string
-	YardLine int
-	Down     int
-	Yfd      int
-}
-
-type GameEvent struct {
-	NflModel
-	Id             int
-	StatsId        string
-	SequenceNumber int    `model:"key"`
-	GameStatsId    string `model:"key"`
-	Type           string
-	ActingTeam     string
-	Summary        string
-	Clock          string
-	GameEventData  GameEventData
-	Data           string
-	CreatedAt      time.Time
-	UpdatedAt      time.Time
 }
