3762b60685cce3297b396abaa40985cc29c7bcb5
Ben Atkin <ben@benatkin.com>
2013-12-12 05:56:20 -0700

    refactor to use WeeklyDigest non-persisted model object

--- webapp/db/schema.rb
+++ webapp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131212123450) do
+ActiveRecord::Schema.define(version: 20131205202613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -495,13 +495,5 @@ ActiveRecord::Schema.define(version: 20131212123450) do
   end
 
   add_index "venues", ["stats_id"], name: "index_venues_on_stats_id", using: :btree
-
-  create_table "weekly_digests", force: true do |t|
-    t.string   "subject"
-    t.datetime "deliver_at"
-    t.datetime "delivered_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
