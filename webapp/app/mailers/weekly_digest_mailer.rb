8adbcf698921d1089269b80b969999e87772dcda
Ben Atkin <ben@benatkin.com>
2013-12-19 04:54:25 -0700

    link to league in weekly update email, which just generates a roster

--- webapp/app/mailers/weekly_digest_mailer.rb
+++ webapp/app/mailers/weekly_digest_mailer.rb
@@ -2,6 +2,7 @@ class WeeklyDigestMailer < ActionMailer::Base
   default from: "Fair Market Fantasy <no-reply@fairmarketfantasy.com>"
 
   def digest_email(user)
+    @base_url = url_for controller: 'pages', action: 'index'
     @user = user
     weekly_digest = WeeklyDigest.new(user: user)
     @rosters = weekly_digest.rosters
