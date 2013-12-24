2bd98e7077c21a2f741faa22b0b9168d9784272e
Mike Ihbe <mihbe@yammer-inc.com>
2013-12-23 19:37:55 -0500

    Add routes for playoff and home (logged in)

--- webapp/app/models/games_market.rb
+++ webapp/app/models/games_market.rb
@@ -1,5 +1,5 @@
 class GamesMarket < ActiveRecord::Base
-  attr_accessible :game_stats_id, :market_id
+  attr_protected
   belongs_to :market, :inverse_of => :games_markets
   belongs_to :game, :foreign_key => 'game_stats_id', :inverse_of => :games_markets
 
