d1fbec220cd46ec42d38eb7e670209dca9b26b08
Mike Ihbe <mihbe@yammer-inc.com>
2014-01-05 11:23:10 -0800

    fix game point scoring

--- webapp/app/models/games_market.rb
+++ webapp/app/models/games_market.rb
@@ -17,11 +17,11 @@ class GamesMarket < ActiveRecord::Base
       winning_mp = market.market_players.where(:player_stats_id => Team.where(:abbrev => game.winning_team).first.players.map(&:stats_id)).first
       losing_mp = market.market_players.where(:player_stats_id => Team.where(:abbrev => game.losing_team).first.players.map(&:stats_id)).first
       StatEvent.create!(:player_stats_id => winning_mp.player_stats_id, :game_stats_id => game.stats_id, :activity => "Win vs #{game.losing_team}", :point_value => 100, :data => '{}')
-      StatEvent.create!(:player_stats_id => winning_mp.player_stats_id, :game_stats_id => game.stats_id, :activity => "Points Scored vs #{game.losing_team}", :point_value => 2 * home['points'], :data => '{}')
-      StatEvent.create!(:player_stats_id => winning_mp.player_stats_id, :game_stats_id => game.stats_id, :activity => "Points Against vs #{game.losing_team}", :point_value => -2 * away['points'], :data => '{}')
+      StatEvent.create!(:player_stats_id => winning_mp.player_stats_id, :game_stats_id => game.stats_id, :activity => "Points Scored vs #{game.losing_team}", :point_value => 2 * game.winning_team_status['points'], :data => '{}')
+      StatEvent.create!(:player_stats_id => winning_mp.player_stats_id, :game_stats_id => game.stats_id, :activity => "Points Against vs #{game.losing_team}", :point_value => -2 * game.losing_team_status['points'], :data => '{}')
 
-      StatEvent.create!(:player_stats_id => losing_mp.player_stats_id, :game_stats_id => game.stats_id, :activity => "Points Scored vs #{game.winning_team}", :point_value => 2 * away['points'], :data => '{}')
-      StatEvent.create!(:player_stats_id => losing_mp.player_stats_id, :game_stats_id => game.stats_id, :activity => "Points Against vs #{game.winning_team}", :point_value => -2 * home['points'], :data => '{}')
+      StatEvent.create!(:player_stats_id => losing_mp.player_stats_id, :game_stats_id => game.stats_id, :activity => "Points Scored vs #{game.winning_team}", :point_value => 2 * game.losing_team_status['points'], :data => '{}')
+      StatEvent.create!(:player_stats_id => losing_mp.player_stats_id, :game_stats_id => game.stats_id, :activity => "Points Against vs #{game.winning_team}", :point_value => -2 * game.winning_team_status['points'], :data => '{}')
       # TODO: Whatever market transformations are needed
       GamesMarket.find_by_sql(["SELECT * FROM finish_elimination_game(?, ?, ?)", self.id, winner, loser])
     end
