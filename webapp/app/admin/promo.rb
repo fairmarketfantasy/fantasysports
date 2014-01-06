c39ae160a13785a41ac21e1e80bd2e13e15abb7f
Mike Ihbe <mihbe@yammer-inc.com>
2014-01-06 12:26:27 -0800

    Fix some admin stuff

--- webapp/app/admin/promo.rb
+++ webapp/app/admin/promo.rb
@@ -4,23 +4,13 @@ ActiveAdmin.register Promo do
 
   index do
     column :id
-=begin
- market_id           | integer                     | not null
- player_id           | integer                     | not null
- shadow_bets         | numeric                     |
- bets                | numeric                     | default 0
- locked_at           | timestamp without time zone |
- initial_shadow_bets | numeric                     |
- locked              | boolean                     | default false
- score               | integer                     | not null default 0
- player_stats_id     | character varying(255)      |
-=end
     column :code
     column :valid_until
-    column(:name) {|mp| mp.player.name }
-    column :initial_shadow_bets
-    column :shadow_bets
-    column :bets
+    column :cents
+    column :tokens
+    column :only_new_users
+    column :created_at
+
     column('number_of_uses') {|promo| promo.users.count }
     default_actions
   end
