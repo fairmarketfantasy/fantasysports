8ec06e1a1f4e5143b8f874d15472e946fa2848b0
Mike Ihbe <mihbe@yammer-inc.com>
2014-01-06 16:13:40 -0800

    schema

--- webapp/db/schema.rb
+++ webapp/db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20140102084910) do
-=======
 ActiveRecord::Schema.define(version: 20140106215610) do
->>>>>>> production
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
