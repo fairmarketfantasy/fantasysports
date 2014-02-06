c52aa44c4e1bc9fffcc8e4dca33e51b057c85e6c
Alexander <alexander.korotkov@easternpeak.com>
2014-02-06 17:54:04 +0200

    'Oops, something went wrong,try again later' error appears on home page without any reason

--- webapp/app/assets/javascripts/controllers/roster_list_controller.js
+++ webapp/app/assets/javascripts/controllers/roster_list_controller.js
@@ -1,9 +1,10 @@
 angular.module("app.controllers")
 .controller('RosterListController', ['$scope', 'rosters', 'markets', 'flash', '$routeParams', function($scope, rosterService, markets, flash, $routeParams) {
   $scope.rosterService = rosterService;
+
   rosterService.fetchMine({sport: $routeParams.sport}).then(function() {
     $scope.rosterList = rosterService.top();
   });
-  rosterService.setPoller(function() { rosterService.fetchMine(); }, 10000);
+  rosterService.setPoller(function() { rosterService.fetchMine({sport: $scope.currentUser.currentSport}); }, 10000);
 }]);
 
