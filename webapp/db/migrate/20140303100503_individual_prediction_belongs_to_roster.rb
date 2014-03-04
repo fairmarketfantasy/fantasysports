244d5aabd6d82284726b8fc65ec0aaed6361fa60
Andrew <andrew.kotov@easternpeak.com>
2014-03-04 16:12:50 +0200

    Fixes prediction create Fixes migration Revert selecting not closed markets

--- webapp/db/migrate/20140303100503_individual_prediction_belongs_to_roster.rb
+++ webapp/db/migrate/20140303100503_individual_prediction_belongs_to_roster.rb
@@ -3,8 +3,8 @@ class IndividualPredictionBelongsToRoster < ActiveRecord::Migration
     rename_column :individual_predictions, :roster_player_id, :roster_id
     add_column :individual_predictions, :player_id, :integer
     add_column :individual_predictions, :user_id, :integer
-    remove_column :individual_predictions, :event_type
-    remove_column :individual_predictions, :value
-    remove_column :individual_predictions, :less_or_more
+    remove_column :individual_predictions, :event_type, :string
+    remove_column :individual_predictions, :value, :integer
+    remove_column :individual_predictions, :less_or_more, :string
   end
 end
