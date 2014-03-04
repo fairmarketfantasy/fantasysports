aaac6cc391cef0254df2532dacac1065f48e88ec
Andrew <andrew.kotov@easternpeak.com>
2014-03-04 19:01:47 +0200

    Adds player name to prediction

--- webapp/app/serializers/individual_prediction_serializer.rb
+++ webapp/app/serializers/individual_prediction_serializer.rb
@@ -1,9 +1,12 @@
 class IndividualPredictionSerializer < ActiveModel::Serializer
-  attributes :id, :player_id, :market_name, :event_predictions
+  attributes :id, :player_id, :market_name, :event_predictions, :player_name
   has_many :event_predictions
 
   def market_name
     object.roster.market.name
   end
 
+  def player_name
+    Player.find(object.player_id).name
+  end
 end
