4a8f64223938e4bcd326d3b7f439f75ddbcbd0be
Alexander <alexander.korotkov@easternpeak.com>
2014-03-05 18:41:23 +0200

    Create confirm modal for individual prediction, new logic for add individual prediction

--- webapp/app/assets/javascripts/controllers/create_prediction_controller.js
+++ webapp/app/assets/javascripts/controllers/create_prediction_controller.js
@@ -2,28 +2,39 @@ angular.module("app.controllers")
 .controller('CreateIndividualPredictionController', ['$scope', 'dialog', 'fs', 'market', 'player','flash', '$routeParams', '$dialog', function($scope, dialog, fs, market, player, flash, $routeParams, $dialog) {
     $scope.market = market;
     $scope.player = player;
-    $scope.difference = '';
-    $scope.point='';
+    $scope.confirmShow = false;
+    var eventData = {};
 
     $scope.playerStats = function(){
         fs.prediction.show(player.stats_id).then(function(data){
-           $scope.point = data;
+           $scope.points = data.events;
+//            $scope.eventData();
         });
     }
 
+    $scope.confirmModal = function(text, point, name) {
+        $scope.confirmShow = true;
+        $scope.confirm = {
+            point: point,
+            diff: text,
+            name: name
+        }
+    }
+
+    $scope.confirmSubmit = function(event){
+
+        $scope.confirmShow = false;
+
+        eventData[event] = $scope.confirm;
+         $scope.events =  eventData;
+
+        console.log($scope.events)
+     }
+
     $scope.predictionSubmit = function(){
 
-        if(!$scope.difference.bound || !$scope.difference.point || !$scope.difference.assist){
-            flash.error("All buttons are required");
-            return;
-        }
-
-        var bound = [$scope.difference.bound, $scope.point.rebounds];
-        var point = [$scope.difference.bound, $scope.point.points];
-        var assist = [$scope.difference.bound, $scope.point.assists];
-
-       fs.prediction.submit($routeParams.roster_id,$routeParams.market_id,player.stats_id, bound, point, assist).then(function(data){
-           flash.success("Individual prediction submitted successfully!")
+       fs.prediction.submit($routeParams.roster_id,$routeParams.market_id,player.stats_id, $scope.events).then(function(data){
+           flash.success("Individual prediction submitted successfully!");
            dialog.close();
        });
     }
@@ -34,21 +45,5 @@ angular.module("app.controllers")
 
     $scope.playerStats();
 
-    $scope.openConfirmPredictionDialog = function() {
-//        var player = player
-        var dialogOpts = {
-            backdrop: true,
-            keyboard: true,
-            backdropClick: true,
-            dialogClass: 'modal modal-confirm-prediction',
-            templateUrl: '/confirm_prediction.html',
-            controller: 'ConfirmPredictionController'
-//            resolve: {
-//                player: function() { return player; },
-//                market: function() {  return rosters.currentRoster.market; }
-//            }
-        };
-        return $dialog.dialog(dialogOpts).open();
-    };
 }]);
 
