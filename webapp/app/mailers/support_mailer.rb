02c6dbde92a5bb7e1804fe2b73097e29bb1d934d
Andrew <andrew.kotov@easternpeak.com>
2014-03-05 20:03:42 +0200

    Changes domain config

--- webapp/app/mailers/support_mailer.rb
+++ webapp/app/mailers/support_mailer.rb
@@ -1,12 +1,12 @@
 class SupportMailer < ActionMailer::Base
-  default from: "Fair Market Fantasy <no-reply@fairmarketfantasy.com>"
+  default from: "Fair Market Fantasy <no-reply@predictthat.com>"
 
   def support_mail(title, email, message)
     @title = title
     @email = email
     @message = message
     envelope = {
-      to: 'support@fairmarketfantasy.com',
+      to: 'support@predictthat.com',
       subject: "New FMF Support Request: #{@title}"
     }
     mail(envelope)
