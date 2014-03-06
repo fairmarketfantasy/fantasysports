15643403c4926ec2f6b5fdcda2b5eef4534c5ccc
Andrew <andrew.kotov@easternpeak.com>
2014-03-06 19:32:00 +0200

    Fixes individual prediction show

--- webapp/app/controllers/individual_predictions_controller.rb
+++ webapp/app/controllers/individual_predictions_controller.rb
@@ -3,12 +3,12 @@ class IndividualPredictionsController < ApplicationController
     player = Player.where(stats_id: params[:player_id]).first
     prediction = current_user.individual_predictions.create(player_id: player.id,
                                                             roster_id: params[:roster_id],
-                                                            market_id: params[:market_id])
+                                                            market_id: params[:market_id],
+                                                            pt: IndividualPrediction::PT)
     params[:events].each do |event|
       event_prediction = prediction.event_predictions.create(event_type: event[:name],
                                                              value: event[:value],
-                                                             diff: event[:diff],
-                                                             pt: IndividualPrediction::PT)
+                                                             diff: event[:diff])
       if event_prediction.errors.any?
         return render :text => k + ' ' + event_prediction.errors.full_messages.join(', '),
           status: :unprocessable_entity
--- webapp/app/models/event_prediction.rb
+++ webapp/app/models/event_prediction.rb
@@ -1,5 +1,5 @@
 class EventPrediction < ActiveRecord::Base
   belongs_to :individual_prediction
   attr_protected
-  validates_presence_of :event_type, :value, :less_or_more
+  validates_presence_of :event_type, :value, :diff
 end
