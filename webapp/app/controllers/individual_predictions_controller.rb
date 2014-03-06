32494f583d5a5ba100c4359b52431fa003442047
Andrew <andrew.kotov@easternpeak.com>
2014-03-06 10:32:01 +0200

    Fix Individual prediction creating

--- webapp/app/controllers/individual_predictions_controller.rb
+++ webapp/app/controllers/individual_predictions_controller.rb
@@ -3,16 +3,13 @@ class IndividualPredictionsController < ApplicationController
     prediction = current_user.individual_predictions.create(player_id: params[:player_id],
                                                             roster_id: params[:roster_id],
                                                             market_id: params[:market_id])
-    params.each do |k, v|
-      event_types = ['assists', 'turnovers', 'rebounds', 'points', '3pt made',
-                     'steals', 'blocks']
-      if event_types.include?(k)
-        event_prediction = prediction.event_predictions.create(event_type: k,
-                                                               value: v[1],
-                                                               less_or_more: v[0])
-        if event_prediction.errors.any?
-          render :text => errors.map(&:full_messages).join(', '), status: :unprocessable_entity and return
-        end
+    params[:events].each do |k, v|
+      event_prediction = prediction.event_predictions.create(event_type: k,
+                                                             value: v[:point],
+                                                             less_or_more: v[:diff])
+      if event_prediction.errors.any?
+        return render :text => k + ' ' + event_prediction.errors.full_messages.join(', '),
+          status: :unprocessable_entity
       end
     end
 
