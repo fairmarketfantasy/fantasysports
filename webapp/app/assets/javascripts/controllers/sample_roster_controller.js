7ac22fd2cb37052fefe48054d2c0cc2c470f4f06
Alexander <alexander.korotkov@easternpeak.com>
2014-03-11 15:11:34 +0200

    transaction  amount, add new background img

--- webapp/app/assets/javascripts/controllers/sample_roster_controller.js
+++ webapp/app/assets/javascripts/controllers/sample_roster_controller.js
@@ -21,6 +21,7 @@ angular.module("app.controllers")
         $scope.roster = undefined;
         fs.rosters.getSample(id, sport).then(function(roster) {
             $scope.roster = roster;
+            console.log($scope.roster)
             $scope.isCurrent(roster.market_id);
         });
     };
