42cb2a57b425779349990a4317bb4b32640ccc09
Alexander <alexander.korotkov@easternpeak.com>
2014-03-13 15:06:17 +0200

    add individual prediction history page count

--- webapp/app/assets/javascripts/controllers/individual_prediction_history_list_controller.js
+++ webapp/app/assets/javascripts/controllers/individual_prediction_history_list_controller.js
@@ -3,19 +3,31 @@ angular.module("app.controllers")
   rosterService.setPoller(null);
   $scope.rosterService = rosterService;
   $scope.pt_history = true;
+  $scope.showMore = false;
+  var page = 0;
+  $scope.rosterList = [];
 
-  rosterService.fetchMinePrediction({sport: $routeParams.sport, historical: true}).then(function(data) {
-    $scope.predictionList = data;
-    _.each($scope.predictionList, function(list){
-      _.each(list.event_predictions, function(data){
-        if(data.diff == 'more'){
-          data.diff = 'over';
-        } else{
-          data.diff = 'under';
-        }
-      })
-     })
-  });
+  $scope.fetchMore = function() {
+    $scope.showMore = false
+    page++;
+    rosterService.fetchMinePrediction({sport: $routeParams.sport, historical: true, page: page}).then(function(data) {
+      _.each($scope.predictionList, function(list){
+        _.each(list.event_predictions, function(data){
+          if(data.diff == 'more'){
+            data.diff = 'over';
+          } else{
+            data.diff = 'under';
+          }
+        })
+      });
+
+      if (data.length > 24) {
+        $scope.showMore = true;
+      }
+        $scope.rosterList = $scope.rosterList.concat(data);
+      });
+  };
+  $scope.fetchMore();
 
 }]);
 
