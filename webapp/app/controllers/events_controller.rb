14d1a8293b5522108c2456f9cf9da1cf5ebdd35b
Andrew Kotov <aviacompany12ak@gmail.com>
2014-03-17 13:55:22 +0200

    Fixes nil error in stats

--- webapp/app/controllers/events_controller.rb
+++ webapp/app/controllers/events_controller.rb
@@ -59,7 +59,7 @@ class EventsController < ApplicationController
     data = []
     total_stats.each do |k, v|
       value = v.to_d / BigDecimal.new(player.total_games)
-      value = value * 0.7 + recent_stats[k]/games_ids.first(5).count * 0.3
+      value = value * 0.7 + (recent_stats[k] || 0.to_d)/games_ids.first(5).count * 0.3
 
       data << { name: k, value: value.round(1) }
     end
