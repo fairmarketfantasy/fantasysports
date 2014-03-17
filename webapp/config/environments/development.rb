f4ea48b7db2ed5e295569efe5fda21418ecb4a51
Andrew Kotov <aviacompany12ak@gmail.com>
2014-03-17 17:38:53 +0200

    Fixes assets

--- webapp/config/environments/development.rb
+++ webapp/config/environments/development.rb
@@ -30,7 +30,7 @@ Fantasysports::Application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  #devise told me to: 
+  #devise told me to:
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :file
   config.action_mailer.file_settings = {
