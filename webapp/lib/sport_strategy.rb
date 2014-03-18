830fdb3397aa9d0dd732f79a603b0d1d7f6f6c28
Andrew Kotov <aviacompany12ak@gmail.com>
2014-03-18 17:44:00 +0200

    Fixes blank screen error

--- webapp/lib/sport_strategy.rb
+++ webapp/lib/sport_strategy.rb
@@ -17,10 +17,10 @@ class NBAStrategy < SportStrategy
     if type == 'single_elimination'
       @sport.markets.where(
           "game_type IS NULL OR game_type ILIKE '%single_elimination'"
-        ).where(['closed_at > ? AND state IN(\'published\', \'opened\')', Time.now.utc - 4.hours]
+        ).where(['closed_at > ? AND state IN(\'published\', \'opened\')', Time.now.utc]
         ).order('closed_at asc').limit(10).select{|m| m.game_type =~ /single_elimination/ }
     else
-      next_market_day = @sport.markets.where(['closed_at > ?', Time.now.utc - 4.hours]).order('closed_at asc').first.closed_at.beginning_of_day
+      next_market_day = @sport.markets.where(['closed_at > ?', Time.now.utc]).order('closed_at asc').first.closed_at.beginning_of_day
       @sport.markets.where(
           ["game_type IS NULL OR game_type = 'regular_season'"]
           ).where(['closed_at > ? AND closed_at <= ?  AND state IN(\'published\', \'opened\')', Time.now.utc, next_market_day + 1.day + 4.hours]
