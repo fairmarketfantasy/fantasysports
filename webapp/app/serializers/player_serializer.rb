35f8e4a8717bf2c0fdc775defa1dab4fbc33613c
Andrew Kotov <aviacompany12ak@gmail.com>
2014-03-19 21:24:59 +0200

    Revert "Fixes PPG"
    This reverts commit eed6c1ddf22196fc0a98c3e487198679fbe8df74.

--- webapp/app/serializers/player_serializer.rb
+++ webapp/app/serializers/player_serializer.rb
@@ -29,8 +29,7 @@ class PlayerSerializer < ActiveModel::Serializer
   end
 
   def ppg
-    played_games_ids = StatEvent.where("activity='points' AND quantity != 0" ).pluck(:game_stats_id)
-    games_ids = Game.where("game_time < now()").where(stats_id: played_games_ids).
+    games_ids = Game.where("game_time < now()").
                      where("(home_team = '#{object[:team] }' OR away_team = '#{object[:team] }')").
                      pluck('DISTINCT stats_id')
     events = StatEvent.where(player_stats_id: object[:stats_id],
