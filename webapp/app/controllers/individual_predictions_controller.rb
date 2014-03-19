05d14114058c27463208f62ad8a2400c45880778
Andrew Kotov <aviacompany12ak@gmail.com>
2014-03-19 17:38:32 +0200

    Adds first_or_create to IP, removes update

--- webapp/app/controllers/individual_predictions_controller.rb
+++ webapp/app/controllers/individual_predictions_controller.rb
@@ -4,14 +4,14 @@ class IndividualPredictionsController < ApplicationController
     raise HttpException.new(402, "Unpaid subscription!") unless current_user.active_account?
 
     player = Player.where(stats_id: params[:player_id]).first
-    prediction = current_user.individual_predictions.create(player_id: player.id,
-                                                            roster_id: params[:roster_id],
-                                                            market_id: params[:market_id],
-                                                            pt: IndividualPrediction::PT)
+    prediction = current_user.individual_predictions.first_or_create(player_id: player.id,
+                                                                     roster_id: params[:roster_id],
+                                                                     market_id: params[:market_id],
+                                                                     pt: IndividualPrediction::PT)
     params[:events].each do |event|
-      event_prediction = prediction.event_predictions.create(event_type: event[:name],
-                                                             value: event[:value],
-                                                             diff: event[:diff])
+      event_prediction = prediction.event_predictions.first_or_create(event_type: event[:name],
+                                                                      value: event[:value],
+                                                                      diff: event[:diff])
       if event_prediction.errors.any?
         return render :text => k + ' ' + event_prediction.errors.full_messages.join(', '),
           status: :unprocessable_entity
@@ -26,26 +26,6 @@ class IndividualPredictionsController < ApplicationController
     prediction = IndividualPrediction.find(params[:id])
 
     render_api_response individual_prediction
-  end
-
-  def update
-    prediction = IndividualPrediction.find(params[:id])
-    event_names = params[:events].map { |h| h[:name] }
-    prediction.event_predictions.each do |e_p|
-      e_p.destroy unless event_names.include?(e_p.event_type)
-    end
-
-    params[:events].each do |event|
-      event_prediction = prediction.event_predictions.find_or_initialize_by(event_type: event[:name])
-      event_prediction.update(event_type: event[:name], value: event[:value],
-                              diff: event[:diff])
-      if event_prediction.errors.any?
-        return render :text => k + ' ' + event_prediction.errors.full_messages.join(', '),
-          status: :unprocessable_entity
-      end
-    end
-
-    render :text => 'Individual prediction updated successfully!', :status => :ok
   end
 
   def mine
