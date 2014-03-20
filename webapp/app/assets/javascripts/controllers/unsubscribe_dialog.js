6bc1e0dab523ee563ea2600a5d0c1f62877d09e4
Alexander <alexander.korotkov@easternpeak.com>
2014-03-20 17:37:16 +0200

    unsubscribe front-end implementation part 2

--- webapp/app/assets/javascripts/controllers/unsubscribe_dialog.js
+++ webapp/app/assets/javascripts/controllers/unsubscribe_dialog.js
@@ -1,8 +1,10 @@
 angular.module("app.controllers")
-.controller('UnsubscribeDialogController', ['$scope', 'dialog', 'flash','fs', '$location', function($scope, dialog, flash, fs, $location) {
+.controller('UnsubscribeDialogController', ['$scope', 'dialog', 'flash','fs', '$location', '$timeout', function($scope, dialog, flash, fs, $location, $timeout) {
 
     $scope.unsubscribe = function() {
         fs.cards.unsubscribe().then(function(){
+            window.location = "/"
+            dialog.close();
             flash.success('You are unsubscribed!');
         })
     }
