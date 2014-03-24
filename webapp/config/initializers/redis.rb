877ecb153d164524513a470f2273363d9dda8a4f
Sergey Kotov <s.kotovskij@gmail.com>
2014-03-24 19:21:31 +0200

    Set proper redis URL for staging

--- webapp/config/initializers/redis.rb
+++ webapp/config/initializers/redis.rb
@@ -1,8 +1,9 @@
 config = {
   'test' => 'redis://:@localhost:1234/',
   'development' => 'redis://:@localhost:1234/',
-  'staging' => 'redis://:@54.201.179.230:1234/',
+  'staging' => 'redis://:@54.186.50.173:1234/',
   'production' => 'redis://:@54.201.72.28:1234/', # This will be problematic with multiple workers
 }
 uri = URI.parse(config[Rails.env])
-Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
+$redis = Redis.new(:host => uri.host, :port => uri.port, :thread_safe => true)
+Resque.redis = $redis
