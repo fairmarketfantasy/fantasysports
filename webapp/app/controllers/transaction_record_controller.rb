01bc283efee18c6073140eab1c4ecaaa03770dba
Andrew <andrew.kotov@easternpeak.com>
2014-04-03 15:38:12 +0300

    Do not show joined grant

--- webapp/app/controllers/transaction_record_controller.rb
+++ webapp/app/controllers/transaction_record_controller.rb
@@ -1,7 +1,7 @@
 class TransactionRecordController < ApplicationController
   def index
     page = params[:page] || 1
-    balance_event_types = %w(payout joined_grant token_buy token_buy_ios revert_transaction
+    balance_event_types = %w(payout token_buy token_buy_ios revert_transaction
                              free_referral_payout paid_referral_payout referred_join_payout deposit withdrawal
                              manual_payout promo monthly_user_balance monthly_taxes monthly_user_entries)
     render_api_response current_user.transaction_records.where(event: balance_event_types).order('id desc').page(page)
