ea709e524d54994572de0fda494485f1bee8ef00
Alexander <alexander.korotkov@easternpeak.com>
2014-04-04 17:17:59 +0300

    Change individual prediction.add PT mouseover

--- webapp/app/assets/javascripts/controllers/create_prediction_controller.js
+++ webapp/app/assets/javascripts/controllers/create_prediction_controller.js
@@ -1,6 +1,7 @@
 angular.module("app.controllers")
-.controller('CreateIndividualPredictionController', ['$scope', 'dialog', 'fs', 'player','flash', '$routeParams', function($scope, dialog, fs, player, flash, $routeParams) {
+.controller('CreateIndividualPredictionController', ['$scope', 'dialog', 'fs', 'player', 'market' ,'flash', '$routeParams', function($scope, dialog, fs, player, market, flash, $routeParams) {
     $scope.player = player;
+    $scope.market = market;
     $scope.confirmShow = false;
     $scope.eventData = {};
     $scope.reloadBackdrop = false;
@@ -12,16 +13,34 @@ angular.module("app.controllers")
         });
     }
 
-    $scope.confirmModal = function(text, point, name, current_bid) {
+    $scope.confirmModal = function(text, point, name, current_bid, current_pt) {
         if(current_bid){return}
         $scope.confirmShow = true;
         $scope.confirm = {
             value: point,
             diff: text,
             name: name,
-            current_bit : text
+            current_bit : text,
+            current_pt : current_pt
         }
     }
+
+
+    var teamsToGames = {};
+    _.each($scope.market.games, function(game) {
+      teamsToGames[game.home_team] = game;
+      teamsToGames[game.away_team] = game;
+    });
+
+    $scope.opponentFor = function(player) {
+      var game = teamsToGames[player.team];
+      return game && _.find([game.home_team, game.away_team], function(team) { return team != player.team; });
+    };
+
+    $scope.isHomeTeam = function(team) {
+      return !teamsToGames[team] || teamsToGames[team].home_team == team;
+    };
+
     $scope.count = 0;
     $scope.confirmSubmit = function(){
         $scope.eventSubmit = [];
