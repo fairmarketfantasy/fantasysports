8af2fd46634457571d4f57cbcc598e9887fc36b0
Andrew <andrew.kotov@easternpeak.com>
2014-04-04 19:24:18 +0300

    Add new instances config for redis

--- webapp/config/initializers/redis.rb
+++ webapp/config/initializers/redis.rb
@@ -1,8 +1,8 @@
 conf = {
   'test' => 'redis://localhost:6379/',
   'development' => 'redis://localhost:6379/',
-  'staging' => 'redis://172.31.32.224:6379/',
-  'production' => 'redis://172.31.32.56:6379/', # This will be problematic with multiple workers
+  'staging' => 'redis://172.31.43.29:6379/',
+  'production' => 'redis://172.31.35.223:6379/', # This will be problematic with multiple workers
 }
 uri = URI.parse(conf[Rails.env])
 $redis = Redis.new(:host => uri.host, :port => uri.port, :thread_safe => true)
