0a91d7abe7cf084efc87da5307e9d817db328ce6
Sergey Kotov <s.kotovskij@gmail.com>
2014-04-25 17:36:06 +0300

    Schedule fetchers

---
+++ webapp/app/workers/mlb_all_schedule_fetcher_worker.rb
@@ -0,0 +1,14 @@
+class MLBAllScheduleFetcherWorker
+  include Sidekiq::Worker
+  include Sidetiq::Schedulable
+
+  recurrence  do
+    minutely(120)
+  end
+
+  sidekiq_options :queue => :mlb_all_schedule_fetcher
+
+  def perform
+    Team.where(:sport_id => Sport.find_by_name('MLB').id).each { |t| TeamScheduleFetcherWorker.perform_async t.stats_id }
+  end
+end
