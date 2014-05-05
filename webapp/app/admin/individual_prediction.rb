478ab34e4595e5bab1cc7a9ce77d3f26bf053418
Andrew <andrew.kotov@easternpeak.com>
2014-05-05 14:40:32 +0300

    Adds individual predictions to admin page Also fixes admin page errors

---
+++ webapp/app/admin/individual_prediction.rb
@@ -0,0 +1,21 @@
+ActiveAdmin.register IndividualPrediction do
+  filter :user_id
+  filter :market_id
+  filter :player_id
+
+  index do
+    column :id
+    column :user_id
+    column :market_id
+    column :player_id
+    column :pt
+    column :award
+    column :created_at
+    column(:event_type) { |ip| ip.event_predictions.last.event_type }
+    column(:diff) { |ip| ip.event_predictions.last.diff }
+    column(:value) { |ip| ip.event_predictions.last.value }
+    column :game_result
+    column :state
+    default_actions
+  end
+end
--- webapp/app/admin/roster.rb
+++ webapp/app/admin/roster.rb
@@ -10,32 +10,32 @@ ActiveAdmin.register Roster do
 =begin
  id               | integer                     | not null default nextval('rosters_id_seq'::regclass)
  owner_id         | integer                     | not null
- created_at       | timestamp without time zone | 
- updated_at       | timestamp without time zone | 
+ created_at       | timestamp without time zone |
+ updated_at       | timestamp without time zone |
  market_id        | integer                     | not null
- contest_id       | integer                     | 
+ contest_id       | integer                     |
  buy_in           | integer                     | not null
  remaining_salary | numeric                     | not null
- score            | integer                     | 
- contest_rank     | integer                     | 
- amount_paid      | numeric                     | 
- paid_at          | timestamp without time zone | 
- cancelled_cause  | character varying(255)      | 
- cancelled_at     | timestamp without time zone | 
+ score            | integer                     |
+ contest_rank     | integer                     |
+ amount_paid      | numeric                     |
+ paid_at          | timestamp without time zone |
+ cancelled_cause  | character varying(255)      |
+ cancelled_at     | timestamp without time zone |
  state            | character varying(255)      | not null
- positions        | character varying(255)      | 
- submitted_at     | timestamp without time zone | 
+ positions        | character varying(255)      |
+ submitted_at     | timestamp without time zone |
  contest_type_id  | integer                     | not null default 0
  cancelled        | boolean                     | default false
- wins             | integer                     | 
- losses           | integer                     | 
- takes_tokens     | boolean                     | 
+ wins             | integer                     |
+ losses           | integer                     |
+ takes_tokens     | boolean                     |
 
 =end
     column(:owner) {|c| c.owner && c.owner.email }
     column :market_id
     column :contest_id
-    column(:contest_type){|c| c.contest_type.name}
+    column(:contest_type){ |c| c.contest_type.try(:name) }
     column :remaining_salary
     column :state
     column :buy_in
