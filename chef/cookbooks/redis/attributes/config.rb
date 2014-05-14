7c377e7b153a23329338721d4d9c047391891038
Sergey Kotov <s.kotovskij@gmail.com>
2014-05-14 20:08:53 +0300

    Fix Redis recipe

--- chef/cookbooks/redis/attributes/config.rb
+++ chef/cookbooks/redis/attributes/config.rb
@@ -16,3 +16,35 @@ default.redis.config.save            = ['900 1', '300 10', '60 10000']
 default.redis.config.activerehashing = true
 default.redis.config.slaveof_ip      = nil
 default.redis.config.slaveof_port    = node.redis.config.port
+
+###
+## the following configuration settings may only work with a recent redis release
+###
+default.redis.config.configure_slowlog       = false
+default.redis.config.slowlog_log_slower_than = 10000
+default.redis.config.slowlog_max_len         = 1024
+
+default.redis.config.configure_maxmemory_samples = false
+default.redis.config.maxmemory_samples = 3
+
+default.redis.config.configure_no_appendfsync_on_rewrite = false
+default.redis.config.no_appendfsync_on_rewrite = false
+
+default.redis.config.configure_list_max_ziplist = false
+default.redis.config.list_max_ziplist_entries = 512
+#default.redis.config.list_max_ziplist_value   = 64
+
+default.redis.config.configure_set_max_intset_entries = false
+default.redis.config.set_max_intset_entries = 512
+
+default.redis.config.configure_zset_max_ziplist_entries = false
+#default.redis.config.zset_max_ziplist_entries = 128
+
+default.redis.config.configure_zset_max_ziplist_value = false
+#default.redis.config.zset_max_ziplist_value = 64
+
+default.redis.config.configure_hash_max_ziplist_entries = false
+#default.redis.config.hash_max_ziplist_entries = 512
+
+default.redis.config.configure_hash_max_ziplist_value = false
+#default.redis.config.hash_max_ziplist_value = 64
