21546f1480838a9d4ee451d168361fea8866baf5
Sergey Kotov <s.kotovskij@gmail.com>
2014-05-16 16:52:43 +0300

    Second games data fetcher

---
+++ webapp/app/workers/unfetched_games_worker.rb
@@ -0,0 +1,12 @@
+class UnfetchedGamesWorker
+
+  include Sidekiq::Worker
+
+  sidekiq_options :queue => :unfetched_games_worker
+
+  recurrence { minutely(15) }
+
+  def perform
+    Game.where(:sport_id => SportStrategy.for('MLB').sport.id).select { |g| g.game_time < Time.now and g.game_time.year == 2014 and g.stat_events.empty? and g.status.downcase != 'postponed' }.uniq.each { |i| GameStatFetcherWorker.perform_async i.stats_id }
+  end
+end
