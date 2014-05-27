47e5f53847bff43749d5b817371c91280f3b100e
Andrew <andrew.kotov@easternpeak.com>
2014-05-27 12:31:25 +0300

    Ensure about mlb games

--- webapp/app/models/sport.rb
+++ webapp/app/models/sport.rb
@@ -3,6 +3,7 @@ class Sport < ActiveRecord::Base
   has_many :contests
   has_many :players
   has_many :teams
+  has_many :games
   attr_accessible :name, :is_active, :playoffs_on
 
   scope :active, -> { where('is_active') }
