dd14e84083e919759cabaf8acd69545b65739769
Sergey Kotov <s.kotovskij@gmail.com>
2014-06-19 20:17:42 +0300

    WCUP-145 Finish group contests for known failed teams

--- webapp/app/serializers/player_serializer.rb
+++ webapp/app/serializers/player_serializer.rb
@@ -26,10 +26,15 @@ class PlayerSerializer < ActiveModel::Serializer
       :swapped_player_name,
       :pt,
       :logo_url,
-      :disable_pt
+      :disable_pt,
+      :remove_pt
 
   def disable_pt
-    Prediction.prediction_made?(stats_id, 'mvp', '', options[:user])
+    Prediction.prediction_made?(stats_id, 'mvp', '', options[:user]) || object.pt <= 15
+  end
+
+  def remove_pt
+    false #  object.pt <= 15
   end
 
   # TODO: fix for NFL when no stats_id
