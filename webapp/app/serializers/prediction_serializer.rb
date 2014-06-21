a320489bc04cc90f978e26eb4751fda7a07eb37c
Sergey Kotov <s.kotovskij@gmail.com>
2014-06-21 14:12:33 +0300

    Hide trade btn when ML-0

--- webapp/app/serializers/prediction_serializer.rb
+++ webapp/app/serializers/prediction_serializer.rb
@@ -29,7 +29,7 @@ class PredictionSerializer < ActiveModel::Serializer
   end
 
   def show_trade
-    !!current_pt and !Prediction.team_plays?(object.stats_id, object.prediction_type)
+    !!current_pt and !Prediction.team_plays?(object.stats_id, object.prediction_type) and current_pt > 15.0
   end
 
   # CHOOSE THE GAME
