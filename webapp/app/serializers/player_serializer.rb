9f477fc82c2175757ee561ee2077e8a057c68b75
Sergey Kotov <s.kotovskij@gmail.com>
2014-06-21 14:15:26 +0300

    Disable PT when ML=0

--- webapp/app/serializers/player_serializer.rb
+++ webapp/app/serializers/player_serializer.rb
@@ -29,7 +29,7 @@ class PlayerSerializer < ActiveModel::Serializer
       :disable_pt
 
   def disable_pt
-    Prediction.prediction_made?(stats_id, 'mvp', '', options[:user])
+    Prediction.prediction_made?(stats_id, 'mvp', '', options[:user]) or pt.to_f <= 15.0 
   end
 
   # TODO: fix for NFL when no stats_id
