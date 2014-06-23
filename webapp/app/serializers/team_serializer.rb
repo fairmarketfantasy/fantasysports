5a007bc30ad793d6739a4870fcb5b9a97131cdf0
Sergey Kotov <s.kotovskij@gmail.com>
2014-06-23 14:09:34 +0300

    APPLY: WCUP-145 Do not show pt for zero moneyline

--- webapp/app/serializers/team_serializer.rb
+++ webapp/app/serializers/team_serializer.rb
@@ -1,15 +1,19 @@
 class TeamSerializer < ActiveModel::Serializer
-  attributes :name, :logo_url, :pt, :stats_id, :disable_pt
+  attributes :name, :logo_url, :pt, :stats_id, :disable_pt, :remove_pt
 
   def stats_id
     object.stats_id
   end
 
   def disable_pt
-    Prediction.prediction_made?(stats_id, options[:type], '', options[:user]) or object.pt(options) <= 15.0 
+    Prediction.prediction_made?(stats_id, options[:type], '', options[:user])
   end
 
   def pt
     object.pt(options)
+  end
+
+  def remove_pt
+    object.pt(options) <= 15
   end
 end
