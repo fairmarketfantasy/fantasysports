1b520e1320fe6f988560f64a12b8af7d96b8176a
Andrew <andrew.kotov@easternpeak.com>
2014-06-24 12:34:27 +0300

    Hot fix for IOS

--- webapp/app/serializers/team_serializer.rb
+++ webapp/app/serializers/team_serializer.rb
@@ -1,12 +1,12 @@
 class TeamSerializer < ActiveModel::Serializer
-  attributes :name, :logo_url, :pt, :stats_id, :disable_pt, :remove_pt
+  attributes :name, :logo_url, :pt, :stats_id, :disable_pt#, :remove_pt
 
   def stats_id
     object.stats_id
   end
 
   def disable_pt
-    Prediction.prediction_made?(stats_id, options[:type], '', options[:user])
+    Prediction.prediction_made?(stats_id, options[:type], '', options[:user]) or object.pt(options) <= 15.0
   end
 
   def pt
